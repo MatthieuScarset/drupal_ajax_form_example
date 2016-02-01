2a1a8f5d31cb91a71f2bb06ef810f3260571622c
Christophe MADEJA <christophe.madeja@orange.com>
2016-02-01 16:52:34 +0100

    suppression anciens fichiers capistrano

--- config/staging/dev.rb
+++
@@ -1,2 +0,0 @@
-set :domain, 'orangepointcomrefonte.obs-ruby.proj.aql.fr'
-server '192.168.129.92', :domain, :app, :web, :primary => true
--- config/staging/recette.rb
+++
@@ -1,2 +0,0 @@
-set :domain, 'recetteclientrefonte.620nm.com'
-server '192.168.129.92', :domain, :app, :web, :primary => true
