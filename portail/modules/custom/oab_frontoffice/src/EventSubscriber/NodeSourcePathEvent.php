1729c1b86e3cf34b87325345a58621411809637b
MADEJA Christophe <christophe.madeja@orange.com>
2016-09-07 17:11:21 +0200

    Installation des modules "pathauto" et "redirect" Paramétrage des modules et création du pattern pour les nodes Création d'un service pour gérer la redirection vers l'alias du contenu si on visualise la page source

---
+++ portail/modules/custom/oab_frontoffice/src/EventSubscriber/NodeSourcePathEvent.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\oab_frontoffice\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Drupal\Core\Url;
+use Drupal\Core\Routing\TrustedRedirectResponse;
+use Drupal\Core\Language\LanguageInterface;
+
+class NodeSourcePathEvent implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   *
+   * Publish the Event.
+   */
+  public static function getSubscribedEvents()
+  {
+    $events[KernelEvents::REQUEST][] = ['onRequest'];
+
+    return $events;
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * redirect source node url to languaged alias
+   */
+  public function onRequest(GetResponseEvent $event)
+  {
+    $attr = $event->getRequest()->attributes;
+
+    if (NULL !== $attr
+    && NULL !== $attr->get('node')
+    && $attr->get('_controller') == '\Drupal\node\Controller\NodeViewController::view')
+    {
+      $node = $attr->get('node');
+
+      if (is_object($node)
+      && get_class($node) === 'Drupal\node\Entity\Node')
+      {
+        $node_language = $node->language();
+        $node_langId = $node_language->getId();
+
+        // if language is undefined, we load the current language page
+        if ($node_langId == LanguageInterface::LANGCODE_NOT_SPECIFIED)
+        {
+          $node_language = \Drupal::languageManager()->getCurrentLanguage();
+        }
+        $url = Url::fromRoute('entity.node.canonical', ['node' => $node->id()], ['language' => $node_language]);
+        $new_url = $url->toString();
+        $current_uri = $_SERVER['REQUEST_URI'];
+
+        if ($new_url != ''
+        && $new_url != $current_uri)
+        {
+          $response = new TrustedRedirectResponse($new_url, 301);
+          $event->setResponse($response);
+        }
+      }
+    }
+  }
+}
