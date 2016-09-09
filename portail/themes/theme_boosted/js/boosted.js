c61a57e05de303cc6c59c546f6e2d09f122e04fb
MADEJA Christophe <christophe.madeja@orange.com>
2016-09-09 11:45:48 +0200

    Mise en place du menu sticky Correction de problème JS (dans boosted et fichiers custom)

--- portail/themes/theme_boosted/js/boosted.js
+++ portail/themes/theme_boosted/js/boosted.js
@@ -63,7 +63,7 @@ MIT Licensed
     }
   })
 
-}(jQuery);
+}(window.jQuery);
 
 /* ========================================================================
  * Bootstrap: alert.js v3.3.6
@@ -158,7 +158,7 @@ MIT Licensed
 
   $(document).on('click.bs.alert.data-api', dismiss, Alert.prototype.close)
 
-}(jQuery);
+}(window.jQuery);
 
 /* ========================================================================
  * Bootstrap: button.js v3.3.6
@@ -3451,25 +3451,27 @@ MIT Licensed
     });
 })(jQuery);
 
-// forms validation fall back for old navigators
-$(document).ready(function () {
-  'use strict';
-  if(typeof validate == 'function')
-  {
-      $('form').validate({
-          errorPlacement: function (error, element) {
-              error.appendTo('label[for=' + $(element).attr('id') + ']');
-          },
-          errorElement: 'em'
-      });
-      // Duplicate Validation Message for Firefox
-      $('#sign-up input:not([type=submit]):not([type=file])[title]').each(function () {
-          var validationMessage = $(this).attr('title');
-          $(this).attr('x-moz-errormessage', validationMessage);
-      });
-      $('form').removeAttr('novalidate');
-  }
-});
++function ($) {
+  // forms validation fall back for old navigators
+  $(document).ready(function () {
+    'use strict';
+    if(typeof validate == 'function')
+    {
+        $('form').validate({
+            errorPlacement: function (error, element) {
+                error.appendTo('label[for=' + $(element).attr('id') + ']');
+            },
+            errorElement: 'em'
+        });
+        // Duplicate Validation Message for Firefox
+        $('#sign-up input:not([type=submit]):not([type=file])[title]').each(function () {
+            var validationMessage = $(this).attr('title');
+            $(this).attr('x-moz-errormessage', validationMessage);
+        });
+        $('form').removeAttr('novalidate');
+    }
+  });
+}(jQuery);
 /* ========================================================================
  * Bootstrap: tab.js v3.3.6
  * http://getbootstrap.com/javascript/#tabs
--- portail/themes/theme_boosted/js/header.js
+++ portail/themes/theme_boosted/js/header.js
@@ -1,5 +1,39 @@
-$('.subnav').affix({
+/*$('.subnav').affix({
       offset: {
         top: $('#navtop').height()
       }
-});	
+});*/
+
+(function ($, Drupal, Bootstrap) {
+  function init_fixed_navbar(){
+    var offset = 5;
+    var $top_menu = $('#main_nav');
+    if(!$top_menu.length) return;
+    var top_menu_offset = $top_menu.offset();
+
+    $(window).scroll(function () {
+
+      if ($(window).scrollTop() > top_menu_offset.top + offset) {
+        $top_menu.addClass('navbar-fixed');
+        $('.main-container').css('margin-top', $top_menu.height() + 20);
+
+        if ($('#navtop').length){
+          $top_menu.css('top', $('#navtop').height() - 5);
+        }
+      } else {
+        $top_menu.removeClass('navbar-fixed');
+        $('.main-container').css('margin-top', 0);
+
+        if ($('#navtop').length){
+          $top_menu.css('top', 0);
+        }
+      }
+    });
+  }
+
+
+  $(document).ready(function () {
+    init_fixed_navbar();
+  });
+
+})(window.jQuery, window.Drupal, window.Drupal.bootstrap);
