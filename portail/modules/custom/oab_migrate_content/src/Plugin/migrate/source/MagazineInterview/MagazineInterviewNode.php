c90121c077e584e5bb331cd42e58770b09f4a617
QWWT2837 <stephanie.jourdan@orange.com>
2016-09-22 15:44:25 +0200

    migration des images du WYSIWYG

--- portail/modules/custom/oab_migrate_content/src/Plugin/migrate/source/MagazineInterview/MagazineInterviewNode.php
+++ portail/modules/custom/oab_migrate_content/src/Plugin/migrate/source/MagazineInterview/MagazineInterviewNode.php
@@ -134,12 +134,10 @@ class MagazineInterviewNode extends SqlBase {
         if (is_object($body_result) && isset($body_result->field_content_value)){
           $body_value = $body_result->field_content_value;
           $body_value = preg_replace(array('@<br>\r\n@', '@<br>\n\r@', '@<br>\n@', '@<br>\r@'), '<br>', $body_value);
-          $body_value = check_markup($body_value, 'full_html');
         }
         elseif (is_array($body_result) && isset($body_result['field_content_value'])){
           $body_value = $body_result['field_content_value'];
           $body_value = preg_replace(array('@<br>\r\n@', '@<br>\n\r@', '@<br>\n@', '@<br>\r@'), '<br>', $body_value);
-          $body_value = check_markup($body_value, 'full_html');
         }
       }
     }
@@ -149,6 +147,8 @@ class MagazineInterviewNode extends SqlBase {
     if ($field_location_value !== '') $verbatim_content .= '<p class="source_verbatim">' . $field_location_value . '</p>';
     if ($field_profil_value !== '') $verbatim_content .= '<p class="source_verbatim">' . $field_profil_value . '</p>';
     $verbatim_content .= '</div>';
+
+    $body_value = oab_migrate_wysiwyg_images($body_value, $row->getSourceProperty('nid'));
 
     $row->setSourceProperty('content_field', array(
       array('value' => $verbatim_content, 'format' => 'full_html', 'zone' => 'default'),
