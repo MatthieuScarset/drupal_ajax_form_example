3f616332a762a081743990a054b812c9b7cfa162
FFLB8539 <amel.benrahma@orange.com>
2016-10-05 18:15:30 +0200

    JIRA_2533 : module de contact en sticky

--- portail/themes/theme_boosted/js/header.js
+++ portail/themes/theme_boosted/js/header.js
@@ -35,9 +35,42 @@
     });
   }
 
+  function init_fixed_contactmodule(){
+    var offset = 0;
+    var $contact_module = $('#contact_module');
+    if(!$contact_module.length) return;
+    var contact_module_offset = $contact_module.offset();
+    var menu_offset = 0;
+
+    if ($('#toolbar-bar').length){
+      menu_offset += $('#toolbar-bar').height();
+    }
+    if ($('#toolbar-item-administration-tray').length){
+      menu_offset += $('#toolbar-item-administration-tray').height();
+    }
+    if ($('#main_nav').length){
+      menu_offset += $('#main_nav').height();
+    }
+
+    $(window).scroll(function () {
+
+      if ($(window).scrollTop() > contact_module_offset.top - menu_offset) {
+        $contact_module.addClass('sticky-module');
+        $('.main-container').css('margin-top', $contact_module.height() + 20);
+        $contact_module.css('top', menu_offset + 10);
+        $('.region-pre-content .affix').css('top', $contact_module.height() + menu_offset);
+      } else {
+        $contact_module.removeClass('sticky-module');
+        $('.main-container').css('margin-top', 0);
+        $contact_module.css('top', 0);
+        $('.region-pre-content .affix').css('top', $('#navbar').height() + menu_offset);
+      }
+    });
+  }
 
   $(document).ready(function () {
     init_fixed_navbar();
+    init_fixed_contactmodule();
   });
 
 })(window.jQuery, window.Drupal, window.Drupal.bootstrap);
