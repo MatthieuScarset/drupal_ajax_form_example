934410e806b4a7eb6d47f714279b662584adb2d1
MADEJA Christophe <christophe.madeja@orange.com>
2016-10-12 18:11:49 +0200

    création d'un block qui gère l'url du plugin JIRA update pour placer le block sur les différents thèmes

---
+++ portail/modules/custom/oab_backoffice/src/Plugin/Block/JiraReportBlock.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\oab_backoffice\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ *
+ * @author WLCQ9089
+ * @Block(
+ *   id = "jira_report_block",
+ *   admin_label = @Translation("JIRA report block"),
+ *   category = @Translation("Blocks")
+ * )
+ *
+ */
+
+class JiraReportBlock extends BlockBase {
+
+  public function build(){
+    $config = $this->getConfiguration();
+    $jira_report_code = isset($config['jira_report_code']) ? $config['jira_report_code'] : '';
+
+    return array(
+      '#markup' => '',
+      '#attached' => array(
+        'library' => array('oab_backoffice/jira'),
+      )
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+
+    // Retrieve existing configuration for this block.
+    $config = $this->getConfiguration();
+
+    // Add a form field to the existing block configuration form.
+    $form['jira_report_code'] = array(
+      '#type' => 'textarea',
+      '#title' => t('Code'),
+      '#rows' => 2,
+      '#description' => t('Paste the entire JIRA javascript url, without the HTML script tag'),
+      '#default_value' => isset($config['jira_report_code']) ? $config['jira_report_code'] : '',
+    );
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockValidate($form, FormStateInterface $form_state) {
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    // Save our custom settings when the form is submitted.
+    $this->setConfigurationValue('jira_report_code', $form_state->getValue('jira_report_code'));
+  }
+}
--- portail/themes/theme_boosted/js/header.js
+++ portail/themes/theme_boosted/js/header.js
@@ -51,27 +51,31 @@
   }
 
   function moveFixedElements(top_menu_offset, offset, top_menu, menu_offset, contact_module_offset, contact_offset, contact_module){
-    if ($(window).scrollTop() > top_menu_offset.top + offset) {
-      top_menu.addClass('navbar-fixed');
-      $('.main-container').css('margin-top', top_menu.height() + 20);
-      top_menu.css('top', menu_offset);
-      $('.region-pre-content .affix').css('top', top_menu.outerHeight() + menu_offset);
-    } else {
-      top_menu.removeClass('navbar-fixed');
-      $('.main-container').css('margin-top', 0);
-      top_menu.css('top', 0);
-      $('.region-pre-content .affix').css('top', $('#navbar').height() + menu_offset);
+    if (top_menu.length) {
+      if ($(window).scrollTop() > top_menu_offset.top + offset) {
+        top_menu.addClass('navbar-fixed');
+        $('.main-container').css('margin-top', top_menu.height() + 20);
+        top_menu.css('top', menu_offset);
+        $('.region-pre-content .affix').css('top', top_menu.outerHeight() + menu_offset);
+      } else {
+        top_menu.removeClass('navbar-fixed');
+        $('.main-container').css('margin-top', 0);
+        top_menu.css('top', 0);
+        $('.region-pre-content .affix').css('top', $('#navbar').height() + menu_offset);
+      }
     }
 
     // module contact
-    if ($(window).scrollTop() > contact_module_offset.top - contact_offset) {
-      contact_module.addClass('sticky-module');
-      $('.main-container').css('margin-top', contact_module.height() + 20);
-      contact_module.css('top', contact_offset);
-    } else {
-      contact_module.removeClass('sticky-module');
-      $('.main-container').css('margin-top', 0);
-      contact_module.css('top', 0);
+    if (contact_module.length) {
+      if ($(window).scrollTop() > contact_module_offset.top - contact_offset) {
+        contact_module.addClass('sticky-module');
+        $('.main-container').css('margin-top', contact_module.height() + 20);
+        contact_module.css('top', contact_offset);
+      } else {
+        contact_module.removeClass('sticky-module');
+        $('.main-container').css('margin-top', 0);
+        contact_module.css('top', 0);
+      }
     }
   }
 
