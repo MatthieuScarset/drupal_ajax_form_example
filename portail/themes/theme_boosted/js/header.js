6b67a140a28f16be66670b646ce034f7f96a2f05
MADEJA Christophe <christophe.madeja@orange.com>
2016-10-12 11:59:13 +0200

    Amélioration des fonction de gestion des éléments sticky de la page (menu, contact)

--- portail/themes/theme_boosted/js/header.js
+++ portail/themes/theme_boosted/js/header.js
@@ -7,70 +7,76 @@
 (function ($, Drupal, Bootstrap) {
   function init_fixed_navbar(){
     var offset = 0;
-    var $top_menu = $('#main_nav');
-    if(!$top_menu.length) return;
-    var top_menu_offset = $top_menu.offset();
+    var top_menu = $('#main_nav');
+    var contact_module = $('#contact_module');
+    if(!top_menu.length || !contact_module.length) return;
+    var top_menu_offset = top_menu.offset();
+    var contact_module_offset = contact_module.offset();
     var menu_offset = 0;
+    var contact_offset = 0;
 
-    if ($('#toolbar-bar').length){
+    if ($('body.toolbar-fixed .toolbar-oriented #toolbar-bar').length) {
       menu_offset += $('#toolbar-bar').height();
+      contact_offset += menu_offset;
     }
-    if ($('#toolbar-item-administration-tray').length){
+    if ($('#toolbar-item-administration-tray.toolbar-tray-horizontal').length) {
       menu_offset += $('#toolbar-item-administration-tray').height();
+      contact_offset += $('#toolbar-item-administration-tray').height();
     }
+    if (top_menu.length) {
+      contact_offset += top_menu.outerHeight();
+    }
+
+    $( window ).resize(function() {
+      menu_offset = 0;
+      contact_offset = 0;
+      if ($('body.toolbar-fixed .toolbar-oriented #toolbar-bar').length) {
+        menu_offset += $('#toolbar-bar').height();
+        contact_offset += menu_offset;
+      }
+      if ($('#toolbar-item-administration-tray.toolbar-tray-horizontal').length) {
+        menu_offset += $('#toolbar-item-administration-tray').height();
+        contact_offset += $('#toolbar-item-administration-tray').height();
+      }
+      if (top_menu.length) {
+        contact_offset += top_menu.outerHeight();
+      }
+
+      moveFixedElements(top_menu_offset, offset, top_menu, menu_offset, contact_module_offset, contact_offset, contact_module);
+    });
 
     $(window).scroll(function () {
-
-      if ($(window).scrollTop() > top_menu_offset.top + offset) {
-        $top_menu.addClass('navbar-fixed');
-        $('.main-container').css('margin-top', $top_menu.height() + 20);
-        $top_menu.css('top', menu_offset);
-        $('.region-pre-content .affix').css('top', $top_menu.height() + menu_offset);
-      } else {
-        $top_menu.removeClass('navbar-fixed');
-        $('.main-container').css('margin-top', 0);
-        $top_menu.css('top', 0);
-        $('.region-pre-content .affix').css('top', $('#navbar').height() + menu_offset);
-      }
+      moveFixedElements(top_menu_offset, offset, top_menu, menu_offset, contact_module_offset, contact_offset, contact_module);
     });
   }
 
-  function init_fixed_contactmodule(){
-    var offset = 0;
-    var $contact_module = $('#contact_module');
-    if(!$contact_module.length) return;
-    var contact_module_offset = $contact_module.offset();
-    var menu_offset = 0;
-
-    if ($('#toolbar-bar').length){
-      menu_offset += $('#toolbar-bar').height();
-    }
-    if ($('#toolbar-item-administration-tray').length){
-      menu_offset += $('#toolbar-item-administration-tray').height();
-    }
-    if ($('#main_nav').length){
-      menu_offset += $('#main_nav').height();
+  function moveFixedElements(top_menu_offset, offset, top_menu, menu_offset, contact_module_offset, contact_offset, contact_module){
+    if ($(window).scrollTop() > top_menu_offset.top + offset) {
+      top_menu.addClass('navbar-fixed');
+      $('.main-container').css('margin-top', top_menu.height() + 20);
+      top_menu.css('top', menu_offset);
+      $('.region-pre-content .affix').css('top', top_menu.height() + menu_offset);
+    } else {
+      top_menu.removeClass('navbar-fixed');
+      $('.main-container').css('margin-top', 0);
+      top_menu.css('top', 0);
+      $('.region-pre-content .affix').css('top', $('#navbar').height() + menu_offset);
     }
 
-    $(window).scroll(function () {
-
-      if ($(window).scrollTop() > contact_module_offset.top - menu_offset) {
-        $contact_module.addClass('sticky-module');
-        $('.main-container').css('margin-top', $contact_module.height() + 20);
-        $contact_module.css('top', menu_offset + 10);
-        $('.region-pre-content .affix').css('top', $contact_module.height() + menu_offset);
-      } else {
-        $contact_module.removeClass('sticky-module');
-        $('.main-container').css('margin-top', 0);
-        $contact_module.css('top', 0);
-        $('.region-pre-content .affix').css('top', $('#navbar').height() + menu_offset);
-      }
-    });
+    // module contact
+    if ($(window).scrollTop() > contact_module_offset.top - contact_offset) {
+      contact_module.addClass('sticky-module');
+      $('.main-container').css('margin-top', contact_module.height() + 20);
+      contact_module.css('top', contact_offset);
+    } else {
+      contact_module.removeClass('sticky-module');
+      $('.main-container').css('margin-top', 0);
+      contact_module.css('top', 0);
+    }
   }
 
   $(document).ready(function () {
     init_fixed_navbar();
-    init_fixed_contactmodule();
   });
 
 })(window.jQuery, window.Drupal, window.Drupal.bootstrap);
