787aa433f8f7f49c0aef2536d413a21fdb9ab80f
MADEJA Christophe <christophe.madeja@orange.com>
2016-10-21 10:19:31 +0200

    Ajout de la clé API "iframely" pour l'intégration de media dans CKEDITOR

--- portail/modules/custom/oab_ckeditor/js/custom_config.js
+++ portail/modules/custom/oab_ckeditor/js/custom_config.js
@@ -11,4 +11,6 @@ CKEDITOR.editorConfig = function(config) {
     CKEDITOR.dtd.$removeEmpty.i = false;
     CKEDITOR.config.removeFormatTags = "b,big,cite,code,del,dfn,em,font,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,u,var";
     delete CKEDITOR.config.coreStyles_italic;
+
+    CKEDITOR.config.embed_provider = '//iframe.ly/api/oembed?url={url}&callback={callback}&api_key=999d16e5ad074182bbe882';
 };
