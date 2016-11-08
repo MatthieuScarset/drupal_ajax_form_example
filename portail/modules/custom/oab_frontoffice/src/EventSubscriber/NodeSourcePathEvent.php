6505a156cdb50add9c8facef8b672bc0d2267810
QWWT2837 <stephanie.jourdan@orange.com>
2016-11-08 17:25:03 +0100

    ajout des noeuds 404 et 403 + conf sur toutes les langues + modif des redirections

--- portail/modules/custom/oab_frontoffice/src/EventSubscriber/NodeSourcePathEvent.php
+++ portail/modules/custom/oab_frontoffice/src/EventSubscriber/NodeSourcePathEvent.php
@@ -31,36 +31,43 @@ class NodeSourcePathEvent implements EventSubscriberInterface {
    */
   public function onRequest(GetResponseEvent $event)
   {
-    $attr = $event->getRequest()->attributes;
-    $is_front_page = \Drupal::service('path.matcher')->isFrontPage();
-
-    if (NULL !== $attr
-    && NULL !== $attr->get('node')
-    && $attr->get('_controller') == '\Drupal\node\Controller\NodeViewController::view'
-    && !$is_front_page)
-    {
-      $node = $attr->get('node');
-
-      if (is_object($node)
-      && get_class($node) === 'Drupal\node\Entity\Node')
+    $request = $event->getRequest();
+    if ($request->attributes->get('exception') instanceof FlattenException) {
+      $code = $request->attributes->get('exception')->getStatusCode();
+      if (!in_array($code, array(403, 404)))
       {
-        $node_language = $node->language();
-        $node_langId = $node_language->getId();
+        $attr = $event->getRequest()->attributes;
+        $is_front_page = \Drupal::service('path.matcher')->isFrontPage();
 
-        // if language is undefined, we load the current language page
-        if ($node_langId == LanguageInterface::LANGCODE_NOT_SPECIFIED)
-        {
-          $node_language = \Drupal::languageManager()->getCurrentLanguage();
-        }
-        $url = Url::fromRoute('entity.node.canonical', ['node' => $node->id()], ['language' => $node_language]);
-        $new_url = $url->toString();
-        $current_uri = $_SERVER['REQUEST_URI'];
+        if (NULL !== $attr
+          && NULL !== $attr->get('node')
+          && $attr->get('_controller') == '\Drupal\node\Controller\NodeViewController::view'
+          && !$is_front_page
+        ) {
+          $node = $attr->get('node');
 
-        if ($new_url != ''
-        && $new_url !== $current_uri)
-        {
-          $response = new RedirectResponse($new_url, 301);
-          $event->setResponse($response);
+          if (is_object($node)
+            && get_class($node) === 'Drupal\node\Entity\Node'
+          ) {
+            $node_language = $node->language();
+            $node_langId = $node_language->getId();
+
+            // if language is undefined, we load the current language page
+            if ($node_langId == LanguageInterface::LANGCODE_NOT_SPECIFIED) {
+              $node_language = \Drupal::languageManager()->getCurrentLanguage();
+            }
+
+            $url = Url::fromRoute('entity.node.canonical', ['node' => $node->id()], ['language' => $node_language]);
+            $new_url = $url->toString();
+            $current_uri = $_SERVER['REQUEST_URI'];
+
+            if ($new_url != ''
+              && $new_url !== $current_uri
+            ) {
+              $response = new RedirectResponse($new_url, 301);
+              $event->setResponse($response);
+            }
+          }
         }
       }
     }
