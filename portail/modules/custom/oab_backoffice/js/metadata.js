c60a991a18518eb556176dd4555f1a3df71c7d3d
Thibaut DESPOULAIN <thibaut.despoulain@orange.com>
2017-03-22 10:43:03 +0100

    J2818 : SEO taille des meta

--- portail/modules/custom/oab_backoffice/js/metadata.js
+++ portail/modules/custom/oab_backoffice/js/metadata.js
@@ -9,24 +9,25 @@
         attach: function (context) {
             var $previewMetaTitle = $("#edit-field-meta-title-0-value");
             $("#edit-title-0-value").keyup(function() {
-
                 var maxTitleChar = $previewMetaTitle.attr("maxlength");
                 if($previewMetaTitle.val().length < maxTitleChar)
                 {
                     $previewMetaTitle.val( this.value )
                     $previewMetaTitle.attr("value" , this.value );
+                }else{
+                    $previewMetaTitle.val( this.value.substr(0, maxTitleChar) );
                 }
-
             });
 
             var $previewMetaDesc = $("#edit-field-meta-description-0-value");
             $("#edit-field-highlight-0-value").keyup(function() {
-
                 var maxDescChar = $previewMetaDesc.attr("maxlength");
                 if($previewMetaDesc.val().length < (maxDescChar-1))
                 {
                     $previewMetaDesc.val( this.value )
                     $previewMetaDesc.attr("value" , this.value );
+                }else{
+                    $previewMetaDesc.val( this.value.substr(0, maxDescChar) );
                 }
 
             });
