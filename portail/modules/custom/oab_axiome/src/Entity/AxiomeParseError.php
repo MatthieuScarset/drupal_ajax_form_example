7905b2d7c0c12e034d01e801a3d89c36711d18d3
Yassine SEMLALI <yassine.semlali@orange.com>
2017-06-08 11:54:15 +0200

    Ajout des tableaux d'erreur

--- portail/modules/custom/oab_axiome/src/Entity/AxiomeParseError.php
+++ portail/modules/custom/oab_axiome/src/Entity/AxiomeParseError.php
@@ -1,11 +1,33 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: cpws0281
- * Date: 07/06/2017
- * Time: 16:49
- */
-class AxiomeParseError {
+namespace Drupal\oab_axiome\Entity;
+
+
+abstract class AxiomeParseErrorEnum extends AxiomeBaseEnum {
+  const MissingAttribute = 'The attribute %key is missing, the import can not be completed.';
+  const MissingKey = 'The key %key is missing, the import can not be completed.';
+  const MissingFile = 'The file %key is missing, the import can not be completed.';
 
 }
+
+/*
+ * EXEMPLE
+  namespace Drupal\oab_axiome\Entity;
+
+  $text = AxiomeParseError::getMessage('MissingKey', array('%key' => 'attributes'));
+  echo $text;
+*/
+
+class AxiomeParseError {
+
+  /**
+   * @param $key
+   * @param array $values
+   * @return \Drupal\Core\StringTranslation\TranslatableMarkup
+   *
+   */
+  public static function getMessage($key, $values = array()){
+    return t(AxiomeParseErrorEnum::getValue($key), $values);
+  }
+}
+
