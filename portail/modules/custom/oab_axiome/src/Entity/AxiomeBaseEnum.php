07fb977ccc8fea17c57a37c7819c9975ba1486b7
Yassine SEMLALI <yassine.semlali@orange.com>
2017-06-08 11:41:16 +0200

    Ajout de tableaux d'erreur

---
+++ portail/modules/custom/oab_axiome/src/Entity/AxiomeBaseEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: cpws0281
+ * Date: 07/06/2017
+ * Time: 17:05
+ */
+class AxiomeBaseEnum {
+
+}
---
+++ portail/modules/custom/oab_axiome/src/Entity/AxiomeParseError.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: cpws0281
+ * Date: 07/06/2017
+ * Time: 16:49
+ */
+class AxiomeParseError {
+
+}
