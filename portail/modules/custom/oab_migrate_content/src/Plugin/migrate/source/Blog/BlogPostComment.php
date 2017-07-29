544a83bdcb005d7a1979daddb96603e07c56614b
QWWT2837 <stephanie.jourdan@orange.com>
2017-07-29 16:17:26 +0200

    corrections pour la migration des commentaires

--- portail/modules/custom/oab_migrate_content/src/Plugin/migrate/source/Blog/BlogPostComment.php
+++ portail/modules/custom/oab_migrate_content/src/Plugin/migrate/source/Blog/BlogPostComment.php
@@ -39,7 +39,7 @@ class BlogPostComment extends SqlBase {
     $query->fields('c', ['cid', 'pid', 'nid', 'uid', 'subject', 'hostname', 'created', 'changed', 'status', 'thread', 'name', 'mail', 'homepage', 'language']);
     $query->fields('b', ['comment_body_value', 'comment_body_format']);
     $query->distinct(TRUE);
-		//$query->condition('n.nid', array(1887,12968), 'IN');
+		//$query->condition('n.nid', array(1887,12968,825,862,868,928,944), 'IN');
     //$query->condition('n.type', 'blog_post');
 		//$query->condition('n.changed', BLOGPOST_SELECT_DATE, '>');
     $query->condition('c.status', 1, '=');
@@ -109,9 +109,7 @@ class BlogPostComment extends SqlBase {
       $row->setSourceProperty('pid_parent', '0');
     }
 
-		$row->setSourceProperty('comment_body', array('value' => 'test', 'format' => 'full_html'));
-		/*$row->setSourceProperty('comment_body_value','test');
-		$row->setSourceProperty('comment_body_format','comments');*/
+		$row->setSourceProperty('comment_body', array('value' => $row->getSourceProperty('comment_body_value'), 'format' => 'comments'));
 
     $row->setSourceProperty('langcode', $row->getSourceProperty('language'));
     $row->setSourceProperty('default_langcode', '1');
