cd587766a23b87edb35aa4a55bf4ea210affb79f
QWWT2837 <stephanie.jourdan@orange.com>
2017-08-01 15:20:15 +0200

    avancement synomia flux

--- portail/modules/custom/oab_synomia_search/modules/oab_synomia_search_flux/src/Controller/OabSynomiaSearchFluxController.php
+++ portail/modules/custom/oab_synomia_search/modules/oab_synomia_search_flux/src/Controller/OabSynomiaSearchFluxController.php
@@ -10,6 +10,7 @@ namespace Drupal\oab_synomia_search_flux\Controller;
 
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\oab_synomia_search_flux\Classes\SynomiaDeletedContent;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -21,20 +22,26 @@ class OabSynomiaSearchFluxController extends ControllerBase
   public function viewFlux(Request $request){
 
 		$response = new Response();
-		$response->setContent("");
-		$response->headers->set('Content-Type','text/xml');
+		$response->setContent($this->getSitemapSynomiaSimple());
+		//$response->headers->set('Content-Type','text/xml');
 		return $response;
-/*
-    $imports = $biObj->getBackbonesImportTable()->fetchAll();
-
-
-    return array(
-      '#lastsImports' => $imports,
-      '#documentation' => $pathDoc,
-      '#variablesForm' => $form,
-      '#theme' => 'backbones_global_settings'
-    );*/
   }
+
+	private function getSitemapSynomiaSimple()
+	{
+		$contentTypes = array();
+
+
+		//calcul de l'intervalle de temps à prendre en compte :
+		// 1. on prend les contenus qui ont été modifiés il y a au moins 12h (pour que le cache Akamai soit rafraichi) :
+		$timeFin = strtotime("-12 hours", time());
+		// 2. on prend les contenus modifiés sur 48h (par rapport a la date précédente) :
+		$timeDebut = strtotime("-48 hours", $timeFin);
+
+
+		$xml_feed = $this->get_sitemap_synomia_by_dates($timeDebut, $timeFin, $contentTypes);
+		return $xml_feed;
+	}
 
   private function getSitemapSynomiaByDates($debut, $fin)
 	{
@@ -47,11 +54,30 @@ class OabSynomiaSearchFluxController extends ControllerBase
 		$timeFin = $endDate->getTimestamp();
 
 		$xml_feed = $this->get_sitemap_synomia_by_dates($timeDebut, $timeFin, $contentTypes);
+		return $xml_feed;
 	}
 
 	private function get_sitemap_synomia_by_dates($timeDebut, $timeFin, $contentTypes)
 	{
+		$host = \Drupal::request()->getSchemeAndHttpHost();
+		$current_language = \Drupal::languageManager()->getCurrentLanguage()->getId();
 
+		/*
+		$test = new SynomiaSearchContent();
+		$test->setNid(139);
+		$test->setContentType('test');
+		$test->setDeleted(time());
+		$test->setLanguage($current_language);
+		$test->setUrl($host);
+		$test->save();
+		$test->delete();
+		*/
+
+		$xml_feed = '';
+		// Création du flux XML
+		$xml_feed .= '<?xml version=\'1.0\' encoding=\'utf-8\'?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+		$xml_feed .= '</urlset>';
+		return "language: ".$current_language. " - debut: ".$timeDebut. ' - fin: '.$timeFin." - base url: ".$host." ".$xml_feed;
 	}
 
 }
