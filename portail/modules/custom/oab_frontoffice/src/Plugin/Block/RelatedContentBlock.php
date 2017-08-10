505e09cc129cb89e50dcd9acf785a0785cf475ac
Christophe MADEJA <christophe.madeja@orange.com>
2017-08-10 11:34:05 +0200

    RUBYPORTAILOBS-3005 Update pour créer le paragraph "related content" + les champs + la relation sur les powerpages + l'ajout du block

---
+++ portail/modules/custom/oab_frontoffice/src/Plugin/Block/RelatedContentBlock.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Drupal\oab_frontoffice\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\node\Entity\Node;
+use Drupal\Core\Entity;
+use Drupal\Core\Url;
+use Drupal\Core\Link;
+use Drupal\image\Entity\ImageStyle;
+
+/**
+ *
+ * @author DMPT2806
+ * @Block(
+ *   id = "related_content_block",
+ *   admin_label = @Translation("Related content"),
+ *   category = @Translation("Blocks"),
+ *   context = {
+ *     "node" = @ContextDefinition(
+ *       "entity:node",
+ *       label = @Translation("Current Node")
+ *     )
+ *   }
+ * )
+ *
+ */
+
+class RelatedContentBlock extends BlockBase {
+
+  public function build(){
+    $content = "";
+    $block = array();
+    // récupération du contexte
+    $node_ctxt = $this->getContextValue('node');
+    $nid_fld = $node_ctxt->nid->getValue();
+    $nid = $nid_fld[0]['value'];
+    // chargement du noeud et de la valeur top zone
+    $node = Node::load($nid);
+    if ($node->hasField('field_related_content')) {
+      $related_content = $node->get('field_related_content')->view('full');
+      $content .= render($related_content);
+      //kint($related_content);
+    }
+
+    $block['#markup'] = $content;
+    return $block;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form = parent::blockForm($form, $form_state);
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockValidate($form, FormStateInterface $form_state) {
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+
+  }
+}
