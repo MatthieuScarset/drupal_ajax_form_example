04441469389829168e208e9ea1c0c2f57da2cd76
QWWT2837 <stephanie.jourdan@orange.com>
2017-08-23 17:50:15 +0200

    ajout lien ajax entre les listes formulaire de la carte + améliorations

--- portail/modules/custom/oab_offices_map/src/Controller/OabOfficesMapPageController.php
+++ portail/modules/custom/oab_offices_map/src/Controller/OabOfficesMapPageController.php
@@ -13,14 +13,18 @@ use Drupal\block\Entity\Block;
 use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Database\Database;
+use Drupal\taxonomy\Plugin\views\wizard\TaxonomyTerm;
 use Drupal\views\Views;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class OabOfficesMapPageController extends ControllerBase {
 
-	public function viewPage(Request $request) {
+	public function getPageTitle(){
+		return t("Our local offices");
+	}
 
+	public function viewPage(Request $request) {
 		//paramètres :
 		$parameters = UrlHelper::filterQueryParameters(\Drupal::request()->query->all());
 		$region_id = (!empty($parameters) && isset($parameters['region']) && $parameters['region'] != 'All') ? $parameters['region'] : 'all';
@@ -41,17 +45,61 @@ class OabOfficesMapPageController extends ControllerBase {
 		$officesListBlock = $officesMapView->buildRenderable('offices_addresses_list_block', array());
 
 		$regionsCountriesForm = \Drupal::formBuilder()->getForm('Drupal\oab_offices_map\Form\OabOfficesMapRegionsCountriesForm');
+
+		$listLabel = t('All offices');
+		if($region_id != 'all'){
+			$region =  \Drupal\taxonomy\Entity\Term::load($region_id);
+			$listLabel = $region->label().' offices';
+		}
 		return array(
 			'#mapBlock' => $mapBlock,
 			'#officesListBlock' => $officesListBlock,
 			'#regionsCountriesForm' => $regionsCountriesForm,
+			'#labelList' => $listLabel,
 			'#theme' => 'offices_map_page',
 			'#attached' => array(
 				'library' =>  array(
 					'oab_offices_map/oab_offices_map.markers'
 				),
+				'drupalSettings' =>  array(
+					'countriesRegionsTab' => $this->getArrayRegionsCountries(),
+				),
 			),
 		);
+	}
+
+	private function getArrayRegionsCountries(){
+		$query = Database::getConnection()->select('node_field_data', 'n');
+		$query->leftjoin('node__field_office_country', 'c', 'n.nid = c.entity_id');
+		$query->leftjoin('taxonomy_term_field_data', 'oc', 'oc.tid = c.field_office_country_target_id');
+		$query->leftjoin('taxonomy_term__field_country_code', 'occ', 'occ.entity_id = c.field_office_country_target_id');
+		$query->leftjoin('node__field_region', 'r', 'n.nid = r.entity_id');
+		$query->leftjoin('taxonomy_term_field_data', 't', 't.tid = r.field_region_target_id');
+		$query->condition('n.type', 'office', '=');
+		$query->condition('n.status', 1, '=');
+		$query->addField('t', 'tid', 'region_tid');
+		$query->addField('t', 'name', 'region_name');
+		$query->addField('oc', 'tid', 'country_tid');
+		$query->addField('oc', 'name', 'country_name');
+		$query->addField('occ', 'field_country_code_value', 'country_code');
+		$results =	$query->execute()->fetchAll();
+		/*
+		$table_regions_countries = array();
+		foreach ($results as $rowCountryRegion){
+			if((!array_key_exists($rowCountryRegion->region_tid, $table_regions_countries)
+					|| !in_array($rowCountryRegion->country_tid, $table_regions_countries[$rowCountryRegion->region_tid]))
+				&& !empty($rowCountryRegion->region_tid)){
+				$table_regions_countries[$rowCountryRegion->region_tid][] = $rowCountryRegion->country_tid;
+			}
+		}*/
+
+		//tableau [pays id] => region id
+		$table_country_region = array();
+		foreach ($results as $rowCountryRegion){
+			$table_country_region[$rowCountryRegion->country_tid] = $rowCountryRegion->region_tid;
+		}
+
+		return $table_country_region;
 	}
 
 }
--- portail/modules/custom/oab_offices_map/src/Form/OabOfficesMapRegionsCountriesForm.php
+++ portail/modules/custom/oab_offices_map/src/Form/OabOfficesMapRegionsCountriesForm.php
@@ -11,6 +11,7 @@ use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Database\Database;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
 
 class OabOfficesMapRegionsCountriesForm extends FormBase {
 	/**
@@ -32,19 +33,27 @@ class OabOfficesMapRegionsCountriesForm extends FormBase {
 		$form['#method'] = 'get';
 		$regions = $this->getRegions();
 		$countries = $this->getCountries();
+		$form['region_label'] = array(
+			'#type' => 'label',
+			'#title' => t('Region')
+		);
 		$form['region'] = [
 			'#multiple_toggle' => '1',
 			'#type' => 'select',
 			'#options' => $regions,
 			'#default_value' => $selectedRegion,
-			'#title' => $this->t('Region')
+			'#attributes' => array('class' => array('select-regions')),
 		];
+		$form['country_label'] = array(
+			'#type' => 'label',
+			'#title' => t('Country')
+		);
 		$form['country'] = [
 			'#multiple_toggle' => '1',
 			'#type' => 'select',
 			'#options' => $countries,
 			'#default_value' => $selectedCountry,
-			'#title' => $this->t('Country'),
+			'#attributes' => array('class' => array('select-countries')),
 		];
 
 		$form['submit'] = [
@@ -53,37 +62,10 @@ class OabOfficesMapRegionsCountriesForm extends FormBase {
 			// Prevent op from showing up in the query string.
 			'#name' => '',
 		];
-
 		return $form;
 	}
 
 
-	private function getArrayRegionsCountries(){
-		$query = Database::getConnection()->select('node_field_data', 'n');
-		$query->leftjoin('node__field_office_country', 'c', 'n.nid = c.entity_id');
-		$query->leftjoin('taxonomy_term_field_data', 'oc', 'oc.tid = c.field_office_country_target_id');
-		$query->leftjoin('taxonomy_term__field_country_code', 'occ', 'occ.entity_id = c.field_office_country_target_id');
-		$query->leftjoin('node__field_region', 'r', 'n.nid = r.entity_id');
-		$query->leftjoin('taxonomy_term_field_data', 't', 't.tid = r.field_region_target_id');
-		$query->condition('n.type', 'office', '=');
-		$query->condition('n.status', 1, '=');
-		$query->addField('t', 'tid', 'region_tid');
-		$query->addField('t', 'name', 'region_name');
-		$query->addField('oc', 'tid', 'country_tid');
-		$query->addField('oc', 'name', 'country_name');
-		$query->addField('occ', 'field_country_code_value', 'country_code');
-		$results =	$query->execute()->fetchAll();
-
-		$table_regions_countries = array();
-		foreach ($results as $rowCountryRegion){
-			if((!array_key_exists($rowCountryRegion->region_tid, $table_regions_countries)
-					|| !in_array($rowCountryRegion->country_tid, $table_regions_countries[$rowCountryRegion->region_tid]))
-				&& !empty($rowCountryRegion->region_tid)){
-				$table_regions_countries[$rowCountryRegion->region_tid][] = $rowCountryRegion->country_tid;
-			}
-		}
-		return $table_regions_countries;
-	}
 	private function getCountries(){
 		$query = Database::getConnection()->select('node_field_data', 'n');
 		$query->leftjoin('node__field_office_country', 'c', 'n.nid = c.entity_id');
@@ -131,7 +113,14 @@ class OabOfficesMapRegionsCountriesForm extends FormBase {
 	/**
 	 * {@inheritdoc}
 	 */
-	public function submitForm(array &$form, FormStateInterface $form_state) {
-		// Form is redirected no need for anything here.
+	public function submitForm(array &$form, FormStateInterface $form_state){
+		/*$current_route = \Drupal::routeMatch()->getRouteName();
+		$input = &$form_state->getUserInput();
+		$option = [
+			'query' => array('region' => $input["region"], 'country' => $input["country"]),
+		];
+		$url = Url::fromRoute($current_route, array(), $option);
+		$form_state->setRedirectUrl($url);
+		*/
 	}
 }
