4cc954fe74e4882af2d3335a4c93fe9d8eb1b16e
QWWT2837 <stephanie.jourdan@orange.com>
2017-08-30 17:43:58 +0200

    avancement Synomia - struccture formulaire, reponse, template

---
+++ portail/modules/custom/oab_synomia_search/modules/oab_synomia_search_engine/src/Classes/SynomiaSearchResponse.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: QWWT2837
+ * Date: 30/08/2017
+ * Time: 17:39
+ */
+namespace Drupal\oab_synomia_search_engine\Classes;
+
+class SynomiaSearchResponse {
+
+	public function __construct($fluxXML){
+
+	}
+}
