9266d241039eef16e6dd631158fbe42ee08ae4c8
Tristan Leuillier <tristan.leuillier@orange.com>
2017-09-21 16:21:32 +0200

    Rajout d'une fonction supprimée par inadvertance

--- portail/modules/custom/oab_frontoffice/src/Twig/OabExtension.php
+++ portail/modules/custom/oab_frontoffice/src/Twig/OabExtension.php
@@ -278,4 +278,23 @@ class OabExtension extends \Twig_Extension {
             'render' => $view->render('block_1')
         ];
     }
+
+  /**
+   * Render a custom date format with Twig
+   * Use the internal helper "format_date" to render the date using the current language for texts
+   *
+   * Permet surtout d'avoir le "Long month name", que ne permet pas la fonction "format_date",
+   * ce qui est utile pour les mois RU qui ont deux formats
+   */
+  public static function formatDate($date, $format) {
+    if ($date_format = \DateTime::createFromFormat('Y-m-d', $date)) {
+      $timestmap = strtotime($date);
+    }elseif (is_a($date, 'Drupal\Core\Datetime\DrupalDateTime') || is_a($date, 'DateTime')){
+      $timestmap = $date->getTimestamp();
+    }else{
+      $timestmap = $date;
+    }
+
+    return \Drupal::service('date.formatter')->format($timestmap, "Node created date", $format);
+  }
 }
