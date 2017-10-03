7ee3d0b32bb53b92fab98e032ac9624efcb28075
Thibaut DESPOULAIN <thibaut.despoulain@orange.com>
2017-10-03 11:05:49 +0200

    orange socialbar

--- portail/themes/theme_boosted/js/social_bar/orange_socialbar_settings.js
+++ portail/themes/theme_boosted/js/social_bar/orange_socialbar_settings.js
@@ -9,10 +9,10 @@ var sharebar_parameters = {
     og_description: 'Partagez cette page', //description of the shared page
     og_image: 'http://pocketprod.com/img/960/b_ricard.jpg', //image used in share
     og_locale: "fr_FR", //language (en_US, fr_FR, es_ES, pl_PL, ro_RO, ar_AR)
-    use_bitly: true, //use bitly service to reduce size of link in tweet
+    use_bitly: false, //use bitly service to reduce size of link in tweet
     bitly_token: 'b65ae4885de2cc131c117f4de075e1f69d26794c', //required if use_bitly is set to true
-    twitter_message: "Hello world ! Just wanted to share this with you : ", //tweet
-    twitter_site: '@orange', //twitter @username
+    twitter_message: "", //tweet
+    twitter_site: '@orangebusiness', //twitter @username
     email_subject: 'Look this website I found !',
     email_message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis qui, nulla ea sunt, repudiandae delectus facere amet laborum. Nostrum architecto sunt explicabo ex tenetur eos velit ipsa illum voluptatem quibusdam!',
     email_popin_title: 'Send an email to a friend !',
