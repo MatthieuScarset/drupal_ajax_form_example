30b86b8fdc966167d7ad2337d585d8d61e1bfa67
Christophe MADEJA <christophe.madeja@orange.com>
2017-11-06 15:06:26 +0100

    RUBYPORTAILOBS-3162 Ajout d'actions pour changer le workflow en masse Désactivation des actions superflues dans la view content Traduction de config d'action

---
+++ portail/modules/custom/oab_backoffice/src/Plugin/Action/NodeWorkbenchPublish.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Drupal\oab_backoffice\Plugin\Action;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Action\ActionBase;
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * Node workbench publish.
+ *
+ * @Action(
+ *   id = "oab_backoffice_node_workbench_publish",
+ *   label = @Translation("Workbench publish content"),
+ *   type = "node"
+ * )
+ */
+class NodeWorkbenchPublish extends ActionBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute($entity = NULL) {
+    $entity->set('moderation_state', array('target_id' => 'published'));
+    $entity->save();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access($object, AccountInterface $account = NULL, $return_as_object = FALSE) {
+    $result = $object->access('update', $account, TRUE)
+    ->andIf(AccessResult::allowedIfHasPermission($account, 'use archived_published transition'))
+    ->andIf(AccessResult::allowedIfHasPermission($account, 'use draft_published transition'))
+    ->andIf(AccessResult::allowedIfHasPermission($account, 'use needs_review_published transition'))
+    ->andIf(AccessResult::allowedIfHasPermission($account, 'use published_published transition'));
+
+    return $return_as_object ? $result : $result->isAllowed();
+  }
+
+}
