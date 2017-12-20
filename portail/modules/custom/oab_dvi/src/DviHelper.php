cc39c9ee6272e0410a8e6fcae6040a1645a291a4
Tristan Leuillier <tristan.leuillier@orange.com>
2017-12-20 15:54:39 +0100

    Ajout d'une classe pour travailler sur les termes de taxo spécifiques à DVI

--- portail/modules/custom/oab_dvi/src/DviHelper.php
+++ portail/modules/custom/oab_dvi/src/DviHelper.php
@@ -7,6 +7,10 @@ use ReflectionClass;
 
 abstract class DviHelper {
 
+################################
+################################
+#################################
+## CONSTANTES
     /**
      * Convention de nommage
      *
@@ -20,106 +24,167 @@ abstract class DviHelper {
     const MS_PUBLIC_SECTOR_FR = "Secteur Public";
 
 
+
+################################
+################################
+################################
+## VARIABLES
+    /**
+     * VARIABLES UTILES DANS DviHelper
+     */
+    private static $default_field_market_segment = 'field_market_segment';
+
+
+################################
+################################
+################################
+## PRIVATE FUNCTIONS
+    /**
+     * Renvoie la liste des constantes définies
+     * @return array liste des contraintes
+     */
     private static function getConstants() {
         $oClass = new ReflectionClass(__CLASS__);
         return $oClass->getConstants();
     }
 
-    private static function getMS_All() {
+    /**
+     * Retourne sous forme de tableau construit selon la langue, tous les termes de taxo de Market Segment
+     * @return array [ LANG => [values], LANG => [$values] ]
+     */
+    private static function getMS_all() {
 
         $return = array();
         foreach (self::getConstants() as $constName => $value) {
-
+            ##Je check si c'est des MarketSegment
+            if (substr($constName, 0,2) == "MS") {
+                $return[substr($constName, strlen($constName)-2,strlen($constName))][] = $value;
+            }
         }
 
-        return [
-            'EN'    => [
-                self::U50_EMPLOYEES_EN,
-                self::PUBLIC_SECTOR_EN
-            ],
-            'FR'    => [
-                self::U50_EMPLOYEES_FR,
-                self::PUBLIC_SECTOR_FR
-            ]
-        ];
+        return $return;
     }
 
+
+################################
+################################
+################################
+## PUBLICS FUNCTIONS
+    /**
+     * Renvoie les termes de taxo sous forme de tableau. Possibilité de trier/filtrer par langue
+     * params :
+     *      - lang : Renvoie les termes de la langue correspondante
+     *      - sort : Renvoie les termes triés par langue
+     * @param array $params
+     * @return array|mixed
+     */
     public static function getMSTerms($params = array()) {
+        $terms = self::getMS_all();
+
         $return = array();
         ##Si on passe un langcode, on renvoie les éléments de la langue
         if (isset($params['lang'])) {
             $lang = strtoupper($params['lang']);
-            $all = self::getAll();
-            if (isset($all[$lang]))
-                $return = $all[$lang];
+            if (isset($terms[$lang]))
+                $return = $terms[$lang];
 
          ## Si on passe un sort, on renvoie les elements triés par lang
         } elseif (isset($params['sort']) && $params['sort'] == "lang") {
-            $return = self::getAll();
+            $return = $terms;
 
         ##Sinon, je renvoie un tableau de tous les elements
         } else {
-            foreach (self::getAll() as $lang => $terms) {
-                $return= array_merge($return, $terms);
+            foreach ($terms as $lang => $langTerms) {
+                $return = array_merge($return, $langTerms);
             }
         }
+
         return $return;
     }
 
-    public static function getMSTaxoTerms() {
-        $terms = self::getMSTerms();
-        $terms_ids = array();
+    /**
+     * Renvoie les ID des termes de taxo de Market Segment
+     * Si une langue est passée en paramètre, renvoie seulement les taxos de la langue
+     *
+     * @param null $lang
+     * @return array
+     */
+    public static function getMSTaxoTermsIds($lang = null) {
+        $return = array();
+        $params = array();
+        if ($lang !== null) {
+            $params["lang"] = $lang;
+        }
+        $default_labels = self::getMSTerms($params);
 
-        foreach ($terms as $key => $term) {
-            //vérification de l'existance du terme pour ce vocabulaire
-            $queryTaxo = \Drupal::entityQuery('taxonomy_term');
-            $queryTaxo->condition('vid', "market_segments");
-            $queryTaxo->condition('name', $term);
-            $entity = $queryTaxo->execute();
-            if ( $entity !== null) {
-                $terms_ids[] = key($entity);
+
+        foreach ($default_labels as $label) {
+            $query = \Drupal::entityQuery('taxonomy_term');
+            $query->condition('vid', 'market_segments');
+            $query->condition('name', $label);
+            if ($lang !== null) {
+                $query->condition('langcode', $lang);
+            }
+            $entity = $query->execute();
+
+            if (!empty($entity)) {
+                $return[] = key($entity);
             }
         }
+
+        return $return;
     }
 
-    public static function isDVIProduct($node){
+    /**
+     * Dit si le node passé en paramètre possède un terme de MS spécifique à DVI
+     * @param \Drupal\node\Entity\Node $node
+     * @param null $fieldName
+     * @return bool
+     */
+    public static function hasMSTaxoTerm(\Drupal\node\Entity\Node $node, $fieldName = null) {
+        $return = false;
+
+        ##Je peux pas attribuer une valeur par défaut issue d'une variable
+        ##Du coup, j'init par défaut à false, et si c'est false, je set avec la valeur par défaut
+        if ($fieldName == null) {
+            $fieldName = self::$default_field_market_segment;
+        }
+
+        $lang = $node->language()->getId();
+        $ms_termsIds = self::getMSTaxoTermsIds($lang);
+
+        //on regarde le produit
+        if ( isset($node->$fieldName)) {
+            //récupération des tag market segment du produit
+            $fieldValue = $node->$fieldName->getValue();
+            if (!empty($fieldValue)) {
+                //pour chaque tag on regarde s'il y en a un dvi
+                foreach ($fieldValue as $values){
+                    if(isset($values['target_id']) && in_array($values['target_id'], $ms_termsIds)){
+                        $return = TRUE;
+                    }
+                }
+            }
+        }
+
+        return $return;
+    }
+
+    /**
+     * Dit si la Node passé en paramètre est un produit DVI ou non (ie. s'il possède un des
+     * termes de la taxo MS qui sont en constantes)
+     *
+     * @param \Drupal\node\Entity\Node $node
+     * @param string $fieldName
+     * @return bool
+     */
+    public static function isDVIProduct(\Drupal\node\Entity\Node $node){
         $dvi_product = FALSE;
+        ## First, on check si le node est bien un produit
         if($node->type->entity->get('type') == "product"){
-            //initialisation des tid DVI
-            $default_labels = array(
-                "fr" => array('- 50 salariés', 'Secteur Public'),
-                "en" => array('- 50 employees', 'Public Sector')
-            );
-            $default_values = array();
-            foreach ($default_labels as $lang => $labels)
-            {
-                $default_values[$lang] = array();
-                foreach ($labels as $label) {
-                    $query = \Drupal::entityQuery('taxonomy_term');
-                    $query->condition('vid', 'market_segments');
-                    $query->condition('name', $label);
-                    $query->condition('langcode', $lang);
-                    $entity = $query->execute();
 
-                    if (!empty($entity)) {
-                        $default_values[$lang][] = array_pop($entity);
-                    }
-                }
-            }
-
-            //on regarde le produit
-            if (isset($node->field_market_segment)) {
-                //récupération des tag market segment du produit
-                $field_market_segment = $node->field_market_segment->getValue();
-                if (!empty($field_market_segment) && count($field_market_segment)>0) {
-                    //pour chaque tag on regarde s'il y en a un dvi
-                    foreach ($field_market_segment as $values){
-                        if(in_array($values['target_id'], $default_values[$node->language()->getId()])){
-                            $dvi_product = TRUE;
-                        }
-                    }
-                }
-            }
+            ##Ensuite, on demande si on a un des terme de taxo de DVI
+            $dvi_product = self::hasMSTaxoTerm($node);
         }
         return $dvi_product;
     }
