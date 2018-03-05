b7a795240505c60ab4eff885f7909846badca600
Tristan Leuillier <tristan.leuillier@orange.com>
2018-03-05 16:02:06 +0100

    Fin de l'export du fichier avec ajout d'un batch pour la generation du fichier + une page de "résultat"

--- portail/modules/custom/oab_backoffice/src/Controller/OabExportFileListController.php
+++ portail/modules/custom/oab_backoffice/src/Controller/OabExportFileListController.php
@@ -1,59 +1,91 @@
 <?php
 namespace Drupal\oab_backoffice\Controller;
 
+define('SESSION_NAME','oab_backoffice.export_file_download' );
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\StackMiddleware\Session;
 use Drupal\Core\TypedData\Plugin\DataType\Uri;
 use Drupal\Core\Url;
-use Drupal\oab_backbones\Classes\BackbonesImport;
-use Drupal\oab_backbones\Classes\BackbonesImportData;
-use Drupal\oab_backbones\Form\FilterPerformanceDataForm;
-use Drupal\oab_backbones\Form\GlobalSettingsForm;
 use Drupal\system\FileDownloadController;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Drupal\user\PrivateTempStoreFactory;
 
-class OabExportFileListController extends ControllerBase
-{
-    private static $dir = "temporary://export_document";
+class OabExportFileListController extends ControllerBase {
+    private static $dir = "public://export_document";
 
-    public function setupBatch(Request $request) {
-        \Drupal::logger('export_file')->notice('Setup batch');
-         $originPath = $request->headers->get('referer');
+    public function runBatch(Request $request) {
+
+        $originPath = $request->headers->get('referer');
+
+        ##Je tente de regarder s'il y a des paramètres dans l'URL
+        # ie. si des filtres ont été ajoutés à la vue
+        $params = [];
+        $originPath_parts = explode("?", $originPath);
+        if (count($originPath_parts) > 1) {
+            $parameters = explode("&", $originPath_parts[1]);
+            foreach ($parameters as $param_string) {
+                $param = explode("=", $param_string);
+                if (isset($param[0]) && isset($param[1])) {
+                    $params[$param[0]] = $param[1];
+                }
+            }
+        }
+
+        ##Création du nom unique du fichier final
         $file_name = date('Ymd-His') . "-document-list-" . rand() . ".csv";
+
+
+        ##Setting up du batch
         $batch = array(
             'title' => t("Export document list"),
             'operations'    => array(
-                array( '\Drupal\oab_backoffice\Controller\OabExportFileListController::exportFileList', array($file_name))
+                array(
+                    '\Drupal\oab_backoffice\Controller\OabExportFileListController::exportFileList',
+                    array($params, $file_name)
+                )
             ),
             'progress_message' => t('Exporting document list'),
-            'finished'      => '\Drupal\oab_backoffice\Controller\OabExportFileListController::returnFile'
         );
 
-        batch_set($batch);
 
-        return batch_process($originPath);
+        ##j'enregistre l'URL d'origine (utilisée pour créer un lien de retour à la fin du batch)
+        $tempstore = \Drupal::service('user.private_tempstore')->get(SESSION_NAME);
+        $tempstore->set('origin_url', $originPath);
+
+        ## Lien de la page à afficher à la fin de la création du fichier
+        $route = \Drupal::url('oab_backoffice.download_file_list');
+
+        batch_set($batch);
+        return batch_process($route);
     }
 
 
-    /** Méthode appelée quand on va voir le détail d'un import en BO
+    /**
+     * Méthode appelée quand on va voir le détail d'un import en BO
      */
-    public static function exportFileList($file_name, &$context) {
+    public function exportFileList($params, $file_name, &$context) {
 
-        \Drupal::logger('export_file')->notice('Debut export');
         ## Recuperation de la vue et du bon display view
         $view = \Drupal\views\Views::getView('document_list');
         $view->setDisplay('document_list_page');
-        #$view->display_handler->options['pager']['type'] = 'none';
+        $view->setExposedInput($params);        # Je set les paramètres recupérés dans l'URL pour
+                                                # mettre les memes filtres que la vue qui s'est affichée
+
+        ## Je supprime le pager pour recuperer tous les éléments
+        $view->display_handler->options['pager']['type'] = 'none';
+
         $view->preview();
+
+
         ## Tableau pour recuperer toutes les infos
         $ret = array();
 
         ##Titres dans la premiere row
         $ret[0] = array();
-
 
         $fields = array(
             'relation'  => array(),
@@ -68,6 +100,9 @@ class OabExportFileListController extends ControllerBase
             }
             $ret[0][] = $field_data->options['label'];
         }
+
+        ##Boucle pour recueperer tous les résultats, et modif de l'affichag
+        # en fonction de chacun, grace à la méthode tout en bas
         $results = $view->result;
         foreach ($results as $result) {
             $row = array();
@@ -112,10 +147,10 @@ class OabExportFileListController extends ControllerBase
         }
         #kint($ret);
 
-        $path = self::$dir."/$file_name";
 
-        if (!file_exists(self::$dir)) {
-            mkdir(self::$dir, 0770, TRUE);
+        ## Je check que le dossier existe
+        if (!file_exists($this->dir)) {
+            mkdir($this->dir, 0770, TRUE);
         }
 
         $toWrite = "";
@@ -123,45 +158,55 @@ class OabExportFileListController extends ControllerBase
             $toWrite .= implode(";", $row);
             $toWrite .= "\r\n";
         }
-        $file = file_save_data($toWrite, $path);
-        //$file->save();
+        $path = $this->dir."/$file_name";
+
+        $file = file_unmanaged_save_data($toWrite, $path);
+
+        #On remonte une erreur si le fichier ne se crée pas
         if ($file === false) {
             echo "Erreur lors de la création du fichier de résultat";
             die();
         }
 
-        $context["results"]['file'] = $file;
-        /*$headers = array(
-            'Content-Type'     => 'text/csv',
-            'Content-Disposition' => 'attachment; filename="'.$file->getFilename().'"'
-        );*/
-        #$uri = new Uri($file->getFileUri());
+        # Je sauvegarde l'uri du fichier
+        $tempstore = \Drupal::service('user.private_tempstore')->get(SESSION_NAME);
+        $tempstore->set('file_path', $file);
 
 
     }
 
 
-    public static function returnFile($success, $results, $operations) {
-      /*  kint($results);
-        kint($success);
-        kint($operations);
-        die();*/
-      $fileName = $results['file']->getFilename();
-      $filePath = self::$dir . "/" ; $fileName;
+    public function endingPage(Request $request) {
 
-        $headers = array(
-            'Content-Type'     => 'text/csv',
-            'Content-Disposition' => 'attachment; filename="'.$fileName.'"',
-            'Content-Length' => filesize($filePath),
-            'Content-Transfer-Encoding' => 'binary',
-            'Expires' => '0'
-        );
-        \Drupal::logger('export_file')->notice('return file');
-        return new BinaryFileResponse(drupal_realpath($results['file']->getFileUri()), 200, $headers);
+        ##Je recupère les deux données sauvées pour l'affichage
+        $tempstore = \Drupal::service('user.private_tempstore')->get(SESSION_NAME);
+        $file_url = $tempstore->get('file_path');
+        $origin = $tempstore->get('origin_url');
+
+        ##S'il en manque une, j'affiche un message
+        if ($file_url === null || $origin === null) {
+            throw new \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException();
+        }
+        $url = file_create_url($file_url);
+
+
+        ##J'affiche le résultat
+        return [
+            "#theme"  => "oab_export_file_list__ending_page",
+            "#file_url"   => $url,
+            "#origin_url"   => $origin
+        ];
 
     }
 
-    private static function getValueFromValueArray($valueArray) {
+    /**
+     * Suppression de tous les fichiers du dossier de sauvegarde
+     */
+    public static function clearStorageDir() {
+
+    }
+
+    private function getValueFromValueArray($valueArray) {
         $ret = "";
 
         ##Gestion des Targetsid
