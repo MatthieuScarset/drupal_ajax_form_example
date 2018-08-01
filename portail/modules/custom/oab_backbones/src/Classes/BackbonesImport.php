023cb92edb55c2338d9537f17bbd2ba81dda7779
wilfried michel <wilfried.michel@orange.com>
2018-08-01 11:45:34 +0200

    RUBYPORTAILOBS-3379 gestion retour code smell

--- portail/modules/custom/oab_backbones/src/Classes/BackbonesImport.php
+++ portail/modules/custom/oab_backbones/src/Classes/BackbonesImport.php
@@ -16,129 +16,122 @@ use Drupal\Core\Database\Database;
  */
 class BackbonesImport
 {
-  public static $TABLE_NAME = 'oab_backbones_import';
+    public static $TABLE_NAME = 'oab_backbones_import';
 
-  public function getHeaderTable(){
-    $header = array(
-      'date' => array('data' => t('Date'), 'field' => 'date', 'sort' => 'desc'),
-      'top_ten' => array('data' => t('Top Ten'), 'field' => 'top_ten'),
-      'status' => array('data' => t('Status'), 'field' => 'status'),
-      'comment' => t('Comments')
-    );
-    return $header;
-  }
-
-  /** Méthode qui retourne le tableau des derniers imports pour le BO */
-  public function getBackbonesImportTable()
-  {
-    $imports = array();
-    if (Database::getConnection()->schema()->tableExists($this::$TABLE_NAME))
+    public function getHeaderTable()
     {
-      $query = Database::getConnection()->select($this::$TABLE_NAME, 'i')
-        ->extend('Drupal\Core\Database\Query\TableSortExtender');
-      $imports = $query->fields('i')
-        ->orderByHeader($this->getHeaderTable())
-        ->range(0, 6)
-        ->execute();
+        $header = array(
+            'date' => array('data' => t('Date'), 'field' => 'date', 'sort' => 'desc'),
+            'top_ten' => array('data' => t('Top Ten'), 'field' => 'top_ten'),
+            'status' => array('data' => t('Status'), 'field' => 'status'),
+            'comment' => t('Comments')
+        );
+        return $header;
     }
-    return $imports;
-  }
 
-  /** Crée ou met à jour un import */
-  public function saveNewImport($date)
-  {
-    if (Database::getConnection()->schema()->tableExists($this::$TABLE_NAME))
+    /** Méthode qui retourne le tableau des derniers imports pour le BO */
+    public function getBackbonesImportTable()
     {
-      $query = Database::getConnection()->merge($this::$TABLE_NAME)
-        ->key(array('date' => $date))
-        ->insertFields(array(
-          'date' => $date,
-          'status' => 0
-        ))
-        ->updateFields(array(
-          'date' => $date
-        ))
-        ->execute();
-    }
-  }
-
-  /** Fait une mise à jour de l'import en BDD (status + commentaire) */
-  public function updateImportInDB($date, $status, $comment){
-    if (Database::getConnection()->schema()->tableExists($this::$TABLE_NAME))
-    {
-      $query = Database::getConnection()->update($this::$TABLE_NAME)
-        ->fields(['status' => $status, 'comment' => $comment])
-        ->condition('date', $date, '=')
-        ->execute();
-    }
-  }
-
-  /** Retourne les 4 dates des derniers imports */
-  public function getLastImportsForSelection()
-  {
-    $imports = array();
-    if (Database::getConnection()->schema()->tableExists($this::$TABLE_NAME))
-    {
-      $query = Database::getConnection()->select($this::$TABLE_NAME, 'i');
-      $results =	$query->fields('i')
-        ->condition('i.status', 1, '=')
-        ->range(0,4)
-        ->orderBy('i.date', 'DESC');
-      $results = $query->execute()->fetchAll();
-
-      if (is_array($results) && count($results) > 0) {
-        foreach ($results as $result)
-        {
-          $imports[$result->date] = substr($result->date, 4, 2) . '/' . substr($result->date, 0, 4);
+        $imports = array();
+        if (Database::getConnection()->schema()->tableExists($this::$TABLE_NAME)) {
+            $query = Database::getConnection()->select($this::$TABLE_NAME, 'i')
+                ->extend('Drupal\Core\Database\Query\TableSortExtender');
+            $imports = $query->fields('i')
+                ->orderByHeader($this->getHeaderTable())
+                ->range(0, 6)
+                ->execute();
         }
-      }
+        return $imports;
     }
-    return $imports;
-  }
 
-  /** Retourne le commentaire saisi pour un import */
-  public function getCommentForImport($date)
-  {
-    $comment = "";
-    if (Database::getConnection()->schema()->tableExists($this::$TABLE_NAME))
+    /** Crée ou met à jour un import */
+    public function saveNewImport($date)
     {
-      $query = Database::getConnection()->select($this::$TABLE_NAME, 'i');
-      $results =	$query->fields('i')
-        ->condition('i.date', $date, '=');
-      $results = $query->execute()->fetchAll();
-
-      if (is_array($results) && count($results) > 0) {
-        foreach ($results as $result)
-        {
-          $comment = $result->comment;
+        if (Database::getConnection()->schema()->tableExists($this::$TABLE_NAME)) {
+            $query = Database::getConnection()->merge($this::$TABLE_NAME)
+                ->key(array('date' => $date))
+                ->insertFields(array(
+                    'date' => $date,
+                    'status' => 0
+                ))
+                ->updateFields(array(
+                    'date' => $date
+                ))
+                ->execute();
         }
-      }
     }
-    return $comment;
-  }
 
-
-  /** Retourne le dernier import validé avant celui passé en paramètre */
-  public function getLastImportBeforeDate($date)
-  {
-    $dateImport = "";
-    if (Database::getConnection()->schema()->tableExists($this::$TABLE_NAME))
+    /** Fait une mise à jour de l'import en BDD (status + commentaire) */
+    public function updateImportInDB($date, $status, $comment)
     {
-      $query = Database::getConnection()->select($this::$TABLE_NAME, 'i');
-      $results =	$query->fields('i')
-        ->condition('i.status', 1, '=')
-        ->condition('i.date', $date, '<')
-        ->range(0,1)
-        ->orderBy('i.date', 'DESC');
-      $results = $query->execute()->fetchAll();
-
-      if (is_array($results) && count($results) > 0) {
-        foreach ($results as $result)
-        {
-          $dateImport = $result->date;
+        if (Database::getConnection()->schema()->tableExists($this::$TABLE_NAME)) {
+            $query = Database::getConnection()->update($this::$TABLE_NAME)
+                ->fields(['status' => $status, 'comment' => $comment])
+                ->condition('date', $date, '=')
+                ->execute();
         }
-      }
     }
-    return $dateImport;
-  }
+
+    /** Retourne les 4 dates des derniers imports */
+    public function getLastImportsForSelection()
+    {
+        $imports = array();
+        if (Database::getConnection()->schema()->tableExists($this::$TABLE_NAME)) {
+            $query = Database::getConnection()->select($this::$TABLE_NAME, 'i');
+            $results = $query->fields('i')
+                ->condition('i.status', 1, '=')
+                ->range(0, 4)
+                ->orderBy('i.date', 'DESC');
+            $results = $query->execute()->fetchAll();
+
+            if (is_array($results) && count($results) > 0) {
+                foreach ($results as $result) {
+                    $imports[$result->date] = substr($result->date, 4, 2) . '/' . substr($result->date, 0, 4);
+                }
+            }
+        }
+        return $imports;
+    }
+
+    /** Retourne le commentaire saisi pour un import */
+    public function getCommentForImport($date)
+    {
+        $comment = "";
+        if (Database::getConnection()->schema()->tableExists($this::$TABLE_NAME)) {
+            $query = Database::getConnection()->select($this::$TABLE_NAME, 'i');
+            $results = $query->fields('i')
+                ->condition('i.date', $date, '=');
+            $results = $query->execute()->fetchAll();
+
+            if (is_array($results) && count($results) > 0) {
+                foreach ($results as $result) {
+                    $comment = $result->comment;
+                }
+            }
+        }
+        return $comment;
+    }
+
+
+    /** Retourne le dernier import validé avant celui passé en paramètre */
+    public function getLastImportBeforeDate($date)
+    {
+        $dateImport = "";
+        if (Database::getConnection()->schema()->tableExists($this::$TABLE_NAME)) {
+            $query = Database::getConnection()->select($this::$TABLE_NAME, 'i');
+            $results = $query->fields('i')
+                ->condition('i.status', 1, '=')
+                ->condition('i.date', $date, '<')
+                ->range(0, 1)
+                ->orderBy('i.date', 'DESC');
+            $results = $query->execute()->fetchAll();
+
+            if (is_array($results) && count($results) > 0) {
+                foreach ($results as $result) {
+                    $dateImport = $result->date;
+                }
+            }
+        }
+        return $dateImport;
+    }
 }
