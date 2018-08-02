4a061fe1271dd42f24b18ca28f15d1f6767fdb28
Tristan Leuillier <tristan.leuillier@orange.com>
2018-08-02 17:12:01 +0200

    Prise en compte des remontées SONAR

--- portail/modules/custom/oab_ckeditor/src/Plugin/Filter/OfficesMapFilter.php
+++ portail/modules/custom/oab_ckeditor/src/Plugin/Filter/OfficesMapFilter.php
@@ -22,120 +22,119 @@ use Drupal\views\Views;
  */
 class OfficesMapFilter extends FilterBase {
 
-	private $pattern = '/\|\|.*?\|\|/s';
-	private $region_id = "";
-	private $country_id = "";
-	private $boolParamsUrl = false;
+    private $pattern = '/\|\|.*?\|\|/s';
+    private $regionId = "";
+    private $countryId = "";
+    private $boolParamsUrl = false;
 
-	public function process($text, $langcode) {
-		//on regarde d'abord s'il y a des paramètres dans l'url
-		$parameters = UrlHelper::filterQueryParameters(\Drupal::request()->query->all());
-		if (!empty($parameters) && isset($parameters['region'])) {
-			$this->boolParamsUrl = true;
-			$this->region_id = $parameters['region'];
-		}
-		else{
-			$this->region_id = 'all';
-		}
-		if (!empty($parameters) && isset($parameters['country'])) {
-			$this->country_id = $parameters['country'];
-			$this->boolParamsUrl = true;
-		}
-		else{
-			$this->country_id = 'all';
-		}
+    public function process($text, $langcode) {
+        //on regarde d'abord s'il y a des paramètres dans l'url
+        $parameters = UrlHelper::filterQueryParameters(\Drupal::request()->query->all());
+        if (!empty($parameters) && isset($parameters['region'])) {
+            $this->boolParamsUrl = true;
+            $this->regionId = $parameters['region'];
+        } else {
+            $this->regionId = 'all';
+        }
 
-		//Pattern de recherche pour une MAP dans un WYSIWYG
-		$searchResults = array();
-		$error = false;
-		//Recherche du pattern
-		while(preg_match($this->pattern, $text, $searchResults) && !$error)
-		{
-			$chaineTrouvee = $searchResults[0];
-			//on teste s'il y a plusieurs maps dans un résultat
-			$count = mb_substr_count($chaineTrouvee, "}||");
-			if ($count > 1)
-			{
-				$pos = strpos($chaineTrouvee, "}||" ); //on cherche la premiere fin
-				$sousChaine = substr($chaineTrouvee, 0, $pos+2);
-				$sousChaineRemplacee = $this->render_offices_map_block($sousChaine);
-				$text = str_replace($sousChaine, $sousChaineRemplacee, $text);
-			}
-			else {
-				//il n'y a qu'une map a remplacer
-				$newChaine = $this->render_offices_map_block($chaineTrouvee);
-				$text = str_replace($searchResults[0], $newChaine, $text);
-			}
-		}
+        if (!empty($parameters) && isset($parameters['country'])) {
+            $this->countryId = $parameters['country'];
+            $this->boolParamsUrl = true;
+        } else {
+            $this->countryId = 'all';
+        }
 
-		$result = new FilterProcessResult($text);
-		$result->addAttachments(
-			array(
-				'library' => ['oab_offices_map/oab_offices_map.markers'],
-				'drupalSettings' => array(
-					'countriesRegionsTab' => getArrayRegionsCountries(),
-					'allCountriesArray' => getCountriesForJS(),
-					'selectedCountryParameter' => $this->country_id,
-					'selectedRegionParameter' => $this->region_id,
-				),
-				)
-		);
-		return $result;
-	}
+        //Pattern de recherche pour une MAP dans un WYSIWYG
+        $search_results = array();
+        $error = false;
+        //Recherche du pattern
+        while (preg_match($this->pattern, $text, $search_results) && !$error) {
+            $chaine_trouvee = $search_results[0];
+            //on teste s'il y a plusieurs maps dans un résultat
+            $count = mb_substr_count($chaine_trouvee, "}||");
+            if ($count > 1) {
+                $pos = strpos($chaine_trouvee, "}||" ); //on cherche la premiere fin
+                $sous_chaine = substr($chaine_trouvee, 0, $pos+2);
+                $sous_chaine_remplacee = $this->render_offices_map_block($sous_chaine);
+                $text = str_replace($sous_chaine, $sous_chaine_remplacee, $text);
+            } else {
+                //il n'y a qu'une map a remplacer
+                $new_chaine = $this->render_offices_map_block($chaine_trouvee);
+                $text = str_replace($search_results[0], $new_chaine, $text);
+            }
+        }
 
-	public function render_offices_map_block($chaineARemplacer) {
-		$newText = "";
-		$chaine_json = str_replace("||", "", $chaineARemplacer);
-		$tag_info = json_decode($chaine_json);
+        $result = new FilterProcessResult($text);
+        $result->addAttachments(
+            array(
+                'library' => ['oab_offices_map/oab_offices_map.markers'],
+                'drupalSettings' => array(
+                    'countriesRegionsTab' => getArrayRegionsCountries(),
+                    'allCountriesArray' => getCountriesForJS(),
+                    'selectedCountryParameter' => $this->countryId,
+                    'selectedRegionParameter' => $this->regionId,
+                ),
+                )
+        );
+        return $result;
+    }
 
-		if (isset($tag_info->block_id) && $tag_info->block_id == "offices_map_block") {
+    public function render_offices_map_block($chaine_a_remplacer) {
+        $new_text = "";
+        $chaine_json = str_replace("||", "", $chaine_a_remplacer);
+        $tag_info = json_decode($chaine_json);
 
-			$parameters = UrlHelper::filterQueryParameters(\Drupal::request()->query->all());
-			if (!$this->boolParamsUrl && !empty($tag_info->region_id)) {
-				$this->region_id = $tag_info->region_id;
-			}
+        if (isset($tag_info->block_id) && $tag_info->block_id == "offices_map_block") {
 
-			if (!$this->boolParamsUrl && !empty($tag_info->country_id)) {
-				$this->country_id = $tag_info->country_id;
-			}
-			$regionsCountriesForm = \Drupal::formBuilder()
-				->getForm('Drupal\oab_offices_map\Form\MapRegionsCountriesForm', array('region_id' => $this->region_id, 'country_id' => $this->country_id));
+            $parameters = UrlHelper::filterQueryParameters(\Drupal::request()->query->all());
+            if (!$this->boolParamsUrl && !empty($tag_info->region_id)) {
+                $this->regionId = $tag_info->region_id;
+            }
 
+            if (!$this->boolParamsUrl && !empty($tag_info->country_id)) {
+                $this->countryId = $tag_info->country_id;
+            }
+            $regions_countries_form = \Drupal::formBuilder()
+                ->getForm(
+                    'Drupal\oab_offices_map\Form\MapRegionsCountriesForm',
+                    array('region_id' => $this->regionId, 'country_id' => $this->countryId)
+                );
 
 
-			//on doit remplacer la chaine par le rendu du block
-			$officesMapView = Views::getView('offices_map_view');
-			$officesMapView->setDisplay('offices_map_block');
-			$officesMapView->args = array( $this->region_id,  $this->country_id);
-			$officesMapView->preExecute();
-			$officesMapView->execute();
-			$mapBlock = $officesMapView->buildRenderable('offices_map_block', array());
 
-			$officesMapView = Views::getView('offices_map_view');
-			$officesMapView->setDisplay('offices_addresses_list_block');
-			$officesMapView->args = array( $this->region_id,  $this->country_id);
-			$officesMapView->preExecute();
-			$officesMapView->execute();
-			$officesListBlock = $officesMapView->buildRenderable('offices_addresses_list_block', array());
-		}
-		$newText = '<div class="officeMapBlock">'.
-			'<div class="region region-content col-md-12 col-sm-12" >'.
-									'<div class="form_filter_regions_countries col col-md-12 col-sm-12">'.
-											render($regionsCountriesForm).
-            			'</div>'.
-            			'<div class="block_principal_offices_map col-lg-12 col-md-12 col-sm-12">'.
-               			' <div class="block_carte col-lg-9 col-md-8 col-sm-12">'.
-												render($mapBlock).
-                		'</div>'.
-                		'<div class="col-lg-3 col-md-4 col-sm-12 addresses-list">'.
-                  		 ' <div class="col-lg-12 col-md-12 col-sm-12 list">'.
-													 render($officesListBlock).'</div>'.
-               				' </div>'.
-           					' </div>'.
-           					' </div>'.
-										'</div>';
+            //on doit remplacer la chaine par le rendu du block
+            $offices_map_view = Views::getView('offices_map_view');
+            $offices_map_view->setDisplay('offices_map_block');
+            $offices_map_view->args = array($this->regionId,  $this->countryId);
+            $offices_map_view->preExecute();
+            $offices_map_view->execute();
+            $map_block = $offices_map_view->buildRenderable('offices_map_block', array());
 
-		return $newText;
-	}
+            $offices_map_view = Views::getView('offices_map_view');
+            $offices_map_view->setDisplay('offices_addresses_list_block');
+            $offices_map_view->args = array($this->regionId,  $this->countryId);
+            $offices_map_view->preExecute();
+            $offices_map_view->execute();
+            $offices_list_block = $offices_map_view->buildRenderable('offices_addresses_list_block', array());
+        }
+        $new_text = '<div class="officeMapBlock">'.
+            '<div class="region region-content col-md-12 col-sm-12" >'.
+                                    '<div class="form_filter_regions_countries col col-md-12 col-sm-12">'.
+                                            render($regions_countries_form).
+                        '</div>'.
+                        '<div class="block_principal_offices_map col-lg-12 col-md-12 col-sm-12">'.
+                        ' <div class="block_carte col-lg-9 col-md-8 col-sm-12">'.
+                                                render($map_block).
+                        '</div>'.
+                        '<div class="col-lg-3 col-md-4 col-sm-12 addresses-list">'.
+                         ' <div class="col-lg-12 col-md-12 col-sm-12 list">'.
+                                                     render($offices_list_block).'</div>'.
+                            ' </div>'.
+                            ' </div>'.
+                            ' </div>'.
+                                        '</div>';
+
+        return $new_text;
+    }
 }
 
