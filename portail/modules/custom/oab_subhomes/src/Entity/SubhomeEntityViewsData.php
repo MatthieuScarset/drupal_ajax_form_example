c9d6a6da84a8f8dc7468abd123cc605cb1c81a9f
Tristan Leuillier <tristan.leuillier@orange.com>
2018-08-17 10:41:42 +0200

    Création d'un module avec une entité spécifique pour les subhomes. Mise en place de l'affichage pour les nouvelles subhomes, et mise en place du template

---
+++ portail/modules/custom/oab_subhomes/src/Entity/SubhomeEntityViewsData.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\oab_subhomes\Entity;
+
+use Drupal\views\EntityViewsData;
+
+/**
+ * Provides Views data for Subhome entity entities.
+ */
+class SubhomeEntityViewsData extends EntityViewsData {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getViewsData() {
+    $data = parent::getViewsData();
+
+    // Additional information for Views integration, such as table joins, can be
+    // put here.
+
+    return $data;
+  }
+
+}
