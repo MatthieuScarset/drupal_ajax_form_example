d133bd61a3a6920d784ede8fa3ae078afe554d94
LEUILLIER Tristan <Tristan.leuillier@orange.com>
2019-02-06 11:35:34 +0100

    Modif du block mediatheque => transfert des textes en conf + ajout de la trad

--- portail/modules/custom/oab_frontoffice/src/Plugin/Block/MediathequeBlock.php
+++ portail/modules/custom/oab_frontoffice/src/Plugin/Block/MediathequeBlock.php
@@ -23,16 +23,22 @@ use Drupal\image\Entity\ImageStyle;
 
 class MediathequeBlock extends BlockBase {
 
-  public function build() {
+    const TEXT = 'mediatheque_text';
+    const LINK = 'mediatheque_link';
+    const LINK_TEXT = 'mediatheque_link_text';
 
-    /*
-     * J'ai enlevé les trad car s'affiche seulement sur des contenus FR pour l'instant
-     */
-    return array(
-      'text' => 'Vous êtes à la recherche d\'images ou de vidéos pour illustrer vos supports de communication ?',
-      'link'  => 'https://mediatheque.orange.com/fr/mediacenter',
-      'link_text' => "En savoir plus"
-    );
+    public function build() {
+
+        // Retrieve existing configuration for this block.
+        $config = $this->getConfiguration();
+        /*
+         * J'ai enlevé les trad car s'affiche seulement sur des contenus FR pour l'instant
+         */
+        return array(
+          'text' => isset($config[self::TEXT]) ? $config[self::TEXT] : '',
+          'link'  => isset($config[self::LINK]) ? $config[self::LINK] : '',
+          'link_text' => isset($config[self::LINK_TEXT]) ? $config[self::LINK_TEXT] : ''
+        );
   }
 
   /**
@@ -40,6 +46,29 @@ class MediathequeBlock extends BlockBase {
    */
   public function blockForm($form, FormStateInterface $form_state) {
     $form = parent::blockForm($form, $form_state);
+
+      // Retrieve existing configuration for this block.
+      $config = $this->getConfiguration();
+
+      // Add a form field to the existing block configuration form.
+      $form[self::TEXT] = array(
+          '#type' => 'textfield',
+          '#title' => t('Text'),
+          '#default_value' => isset($config[self::TEXT]) ? $config[self::TEXT] : ''
+      );
+
+      $form[self::LINK] = array(
+          '#type' => 'textfield',
+          '#title' => t('Link'),
+          '#default_value' => isset($config[self::LINK]) ? $config[self::LINK] : ''
+      );
+
+      $form[self::LINK_TEXT] = array(
+          '#type' => 'textfield',
+          '#title' => t('Link text'),
+          '#default_value' => isset($config[self::LINK_TEXT]) ? $config[self::LINK_TEXT] : ''
+      );
+
     return $form;
   }
 
@@ -54,6 +83,8 @@ class MediathequeBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function blockSubmit($form, FormStateInterface $form_state) {
-
+      $this->setConfigurationValue(self::TEXT, $form_state->getValue(self::TEXT));
+      $this->setConfigurationValue(self::LINK, $form_state->getValue(self::LINK));
+      $this->setConfigurationValue(self::LINK_TEXT, $form_state->getValue(self::LINK_TEXT));
   }
 }
