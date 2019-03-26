815c700084adfdab6faada437dc9507d361eabcd
LEUILLIER Tristan <Tristan.leuillier@orange.com>
2019-03-26 11:49:54 +0100

    Modif des classe lors de l'import axiome pour correspondre

--- portail/modules/custom/oab_axiome/src/AxiomeContentImporter.php
+++ portail/modules/custom/oab_axiome/src/AxiomeContentImporter.php
@@ -79,33 +79,11 @@ class AxiomeContentImporter {
     }
 
     /**
-     * @param \DOMDocument $dom
-     * @param string $class
-     * @return \DOMNode|bool
-     */
-    private static function findDomByClass($dom, $class, $type = 'div') {
-        $ret = false;
-
-        /** @var \DOMNodeList $elements */
-        $elements = $dom->getElementsByTagName($type);
-        for ($i = 0; $i < $elements->length; $i++) {
-            if (is_a($elements->item($i), 'DOMElement')
-                && $elements->item($i)->hasAttribute('class')
-                && strstr($elements->item($i)->getAttribute('class'), $class)) {
-                $ret = $elements->item($i);
-            }
-        }
-
-        return $ret;
-    }
-
-    /**
      * @param DOMDocument $dom
      * @param $banner_data
      */
     private static function replaceLeftBlock(&$dom, $banner_data) {
         $css_class_left_block =  $banner_data['orange_theme']['boosted_css_name'];
-
         // Si c'est un string, on check la correspondance et tout le reste pour afficher la popup
         // Si y a rien, c'est un tableau qui est renvoyé, on supprime alors la part de l'HTML qui lui est reservé
         if (is_string($css_class_left_block)) {
@@ -120,7 +98,6 @@ class AxiomeContentImporter {
             if ($class_left_block !== '' && isset($correspondances[$class_left_block])) {
                 $title_left_block = $correspondances[$class_left_block];
             }
-       // $title_left_block = \Drupal::config(OabAxiomeSettingsForm::getConfigName())->get($class_left_block);
 
             // change class name
             $nodes = self::getNodesByClass($dom, 'icon-frame-connectivity', 'div');
@@ -136,51 +113,106 @@ class AxiomeContentImporter {
                 $el->setAttribute('style', 'color: '.$font_color);
             }
         } else {
-            if ($dom->hasChildNodes()) {
-                $i = 0;
-                $childToSupp = self::findDomByClass($dom, 'leftcol');
 
-               if ($childToSupp !== false) {
-                   $dom->removeChild($childToSupp);
-               }
+            // On hide la col de gauche
+            $nodes = self::getNodesByClass($dom, 'leftcol', 'div');
+            foreach ($nodes as $el) {
+                $el->removeAttribute('class');
+                $el->setAttribute('class', 'leftcol hidden');
+            }
 
+            // Remove default text of frame
+            $nodes_span = self::getNodesByClass($dom, 'frame', 'span');
+            foreach ($nodes_span as $el) {
+                $el->textContent = "";
+            }
+
+            // Remove frame class
+            $nodes = self::getNodesByClass($dom, 'icon-frame-connectivity', 'div');
+            foreach ($nodes as $el) {
+                $el->removeAttribute('class');
+            }
+
+
+            // Si y a pas de bloc à gauche, on aggrandit le bloc central
+            $nodes = self::getNodesByClass($dom, 'centercol', 'div');
+            foreach ($nodes as $el) {
+                $el->removeAttribute('class');
+                $el->setAttribute('class', 'col col-lg-9 col-md-9 col-sm-9 col-xs-10 centercol');
+            }
+
+            // On modifie aussi le bloc central qui remplace le col-offset
+            $nodes = self::getNodesByClass($dom, 'empty-div-instead-of-col-offset', 'div');
+            foreach ($nodes as $el) {
+                $el->removeAttribute('class');
+                $el->setAttribute('class', 'col col-lg-1 hidden-md hidden-sm empty-div-instead-of-col-offset');
             }
 
         }
     }
 
     private static function replaceCenterBlock(&$dom, $banner_data) {
-        //$title_center = is_string($banner_data['insight']) ? $banner_data['insight'] : "";
-        $title_center = $banner_data['insight'];
-        $font_color = $banner_data['font_color'];
-        // change title
-        $nodes_span = self::getNodesByClass($dom, 'titre3');
-        foreach ($nodes_span as $el) {
-            $el->textContent = $title_center;
-            $el->setAttribute('style', 'color: '.$font_color);
+        if (is_string($banner_data['insight'])) {
+            $title_center = $banner_data['insight'];
+
+            $font_color = $banner_data['font_color'];
+            // change title
+            $nodes_span = self::getNodesByClass($dom, 'titre3');
+            foreach ($nodes_span as $el) {
+                $el->textContent = $title_center;
+                $el->setAttribute('style', 'color: '.$font_color);
+            }
+        } else {
+            // Si y a pas, on vire le texte par défaut, et on cache la div complète
+            $nodes_span = self::getNodesByClass($dom, 'titre3');
+            foreach ($nodes_span as $el) {
+                $el->textContent = "";
+            }
         }
 
     }
 
     private static function replaceRightBlock(&$dom, $banner_data) {
         $css_class_right_block =  $banner_data['pop_out_color']['boosted_css_name'];
-        $title_right_block = is_string($banner_data['offre_name']) ? $banner_data['offre_name'] : "";
-        $popout_color = $banner_data['pop_out_color']['color'];
-        // change class name
-        $nodes = self::getNodesByClass($dom, 'icon-popout-connectivity');
-        foreach ($nodes as $el) {
-            $el->removeAttribute('class');
-            $el->setAttribute('class', 'frame-icon-rel inline text_orange '.$css_class_right_block);
-            $el->setAttribute('style', 'color: '.$popout_color.' !important');
-        }
 
-        // change title
-        $nodes_span = self::getNodesByClass($dom, 'popout', 'span');
-        foreach ($nodes_span as $el) {
-            $el->textContent = $title_right_block;
-            // hack pour mettre en blanc qd le fond est noir
-            if ($popout_color == '#000000') {
-                $el->setAttribute('style', 'color: #FFF !important');
+        if (is_string($banner_data['pop_out_color']['boosted_css_name'])) {
+            $title_right_block = $banner_data['offre_name'];
+            $popout_color = $banner_data['pop_out_color']['color'];
+            // change class name
+            $nodes = self::getNodesByClass($dom, 'icon-popout-connectivity');
+            foreach ($nodes as $el) {
+                $el->removeAttribute('class');
+                $el->setAttribute('class', 'frame-icon-rel inline text_orange '.$css_class_right_block);
+                $el->setAttribute('style', 'color: '.$popout_color.' !important');
+            }
+
+            // change title
+            $nodes_span = self::getNodesByClass($dom, 'popout', 'span');
+            foreach ($nodes_span as $el) {
+                $el->textContent = $title_right_block;
+                // hack pour mettre en blanc qd le fond est noir
+                if ($popout_color == '#000000') {
+                    $el->setAttribute('style', 'color: #FFF !important');
+                }
+            }
+        } else {
+            // On hide la col de gauche
+            $nodes = self::getNodesByClass($dom, 'rightcol', 'div');
+            foreach ($nodes as $el) {
+                $el->removeAttribute('class');
+                $el->setAttribute('class', 'rightcol hidden');
+            }
+
+            // Remove default text of popout
+            $nodes_span = self::getNodesByClass($dom, 'popout', 'span');
+            foreach ($nodes_span as $el) {
+                $el->textContent = "";
+            }
+
+            // Remove frame class
+            $nodes = self::getNodesByClass($dom, 'icon-popout-connectivity', 'div');
+            foreach ($nodes as $el) {
+                $el->removeAttribute('class');
             }
         }
 
