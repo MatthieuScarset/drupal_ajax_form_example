08868ba2075e8772e5ffae87000a6713c865052b
BUET Vincent <VZJS7359@ad-subs.w2k.francetelecom.fr>
2019-09-24 17:32:29 +0200

    RUBYPORTAILOBS-3546 mise en place de la video embed en modal

--- portail/modules/custom/oab_ckeditor/js/plugins/videoembed/lang/en.js
+++ portail/modules/custom/oab_ckeditor/js/plugins/videoembed/lang/en.js
@@ -3,6 +3,7 @@ CKEDITOR.plugins.setLang('videoembed', 'en', {
     button: 'Video Embedded',
     onlytxt: 'Youtube, Vimeo & Dailymotion URLs only',
     validatetxt: 'URL field should not be empty!',
-    input_css: 'Custom CSS class name :'
+    input_css: 'Custom CSS class name :',
+    is_modal: 'Put the video in the modal'
 });
 
