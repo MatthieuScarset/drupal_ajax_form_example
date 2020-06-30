bf50b14ec066795016a642fa68d9e56d9f9b4734
ZEGOUR Malik <malik.zegour@orange.com>
2020-06-30 15:50:04 +0200

    tentative correction mauvais affichage tuiles twitter

--- portail/themes/theme_boosted/js/vision_business.js
+++ portail/themes/theme_boosted/js/vision_business.js
@@ -36,6 +36,17 @@
 
   $(window).on('load', function() {
     grid = createGrid();
+
+    // on capte l'évènement twitter pour recharger le grid une fois l'encart chargé
+    if(twttr !== undefined) {
+      twttr.ready(function (twttr) {
+        twttr.events.bind('rendered', function (event) {
+          // At this point the tweet as been fully loaded
+          // and rendered and you we can proceed with our Javascript
+          updateGrid();
+        });
+      });
+    }
   });
 
   //Par défaut, sur le filter parce qu'on y fait des actions, contrairement au load more
