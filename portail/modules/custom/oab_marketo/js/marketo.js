1b2cac180875161160fc068ce9f8e64687565ef9
Tristan LEUILLIER <tristan.leuillier@orange.com>
2020-07-16 17:23:55 +0200

    Petites modifs pour tealium

--- portail/modules/custom/oab_marketo/js/marketo.js
+++ portail/modules/custom/oab_marketo/js/marketo.js
@@ -1,14 +1,18 @@
 (function ($, Drupal, Bootstrap) {
   $(document).ready(function () {
 
-    var formParameters = drupalSettings.data_for_construction_webform_marketo;
+    if (typeof drupalSettings.marketo_data !== 'undefined') {
+      instantiateMktoForm(drupalSettings.marketo_data.mktoFormID, drupalSettings.marketo_data);
+    }
 
-    if (formParameters != null) {
-      instantiateMktoForm(formParameters.mktoFormID, formParameters);
+    if (typeof utag_data !== 'undefined'
+        && typeof drupalSettings.marketo_data_for_tealium !== 'undefined') {
+      utag_data.version = drupalSettings.marketo_data_for_tealium.version;
+      utag_data.sous_domaine = drupalSettings.marketo_data_for_tealium.sous_domaine;
+      utag_data.univers_affichage = drupalSettings.marketo_data_for_tealium.univers_affichage;
+      utag_data.id_article = drupalSettings.marketo_data.mktPdfName;
     }
 
   });
-
-
 
 })(window.jQuery, window.Drupal, window.Drupal.bootstrap);
