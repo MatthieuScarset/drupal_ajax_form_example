3eb779dc5317a5b226eab3b4c953140ce05d4621
Tristan LEUILLIER <tristan.leuillier@orange.com>
2020-07-29 11:45:50 +0200

    MAJ Capi for test

--- config/deploy/test.rb
+++ config/deploy/test.rb
@@ -2,3 +2,4 @@ set :stage, :test
 set :branch, 'MEP_20200730'
 server '10.36.204.17', domain: 'recetteclientrefonte.obs-ruby.build.620nm.net', user: 'oab_web'
 set :deploy_to, '/var/www/test'
+set :docker_file, '/etc/docker/server/test/docker-compose.yml'
