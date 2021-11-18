cf1ed077a553bff4b30bd0d608e7b71d28d8e7e7
LEUILLIER Tristan <tristan.leuillier@orange.com>
2021-11-18 13:46:03 +0100

    Update NodeSourcePathEvent.php - Add missing use

--- modules/custom/oab_frontoffice/src/EventSubscriber/NodeSourcePathEvent.php
+++ modules/custom/oab_frontoffice/src/EventSubscriber/NodeSourcePathEvent.php
@@ -4,6 +4,7 @@ namespace Drupal\oab_frontoffice\EventSubscriber;
 
 use Drupal\Core\Http\Exception\CacheableAccessDeniedHttpException;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
