829d860f4f8e9b1f6c9af238d71a5007edd39e3d
sjourdan <stephanie.jourdan@orange.com>
2021-11-30 17:05:57 +0100

    RUBYPORTAILOBS-3810 > ajout traduction du message didomi

--- modules/custom/oab_didomi/src/EventSubscriber/IframeChangeSrcEvent.php
+++ modules/custom/oab_didomi/src/EventSubscriber/IframeChangeSrcEvent.php
@@ -4,6 +4,7 @@ namespace Drupal\oab_didomi\EventSubscriber;
 
 use Drupal\block_content\Entity\BlockContent;
 use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Language\LanguageManager;
 use Drupal\Core\Render\Renderer;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
@@ -18,10 +19,22 @@ use Symfony\Component\HttpKernel\KernelEvents;
  */
 class IframeChangeSrcEvent implements EventSubscriberInterface {
 
+
+  /**
+   * @var LanguageManager
+   */
+  private $languageManager;
+  /**
+   * @var Renderer
+   */
   private $renderer;
+  /**
+   * @var EntityTypeManager
+   */
   private $entityTypeManager;
 
-  public function __construct(Renderer $renderer, EntityTypeManager $entityTypeManager) {
+  public function __construct(Renderer $renderer, EntityTypeManager $entityTypeManager, LanguageManager $languageManager) {
+    $this->languageManager = $languageManager;
     $this->renderer = $renderer;
     $this->entityTypeManager = $entityTypeManager;
   }
@@ -50,20 +63,41 @@ class IframeChangeSrcEvent implements EventSubscriberInterface {
 
       //on load le block didomi contenant le message à afficher et le bouton pour accepter le cookie
       $block_didomi = $this->entityTypeManager->getStorage('block_content')->loadByProperties(['uuid' => 'eb454a17-56da-4d21-b3ca-565449d7c452' ]);
+
       if(count($block_didomi) > 0) {
         $block_didomi = reset($block_didomi);
+        /** @var BlockContent $block_didomi */
+        $languages = $block_didomi->getTranslationLanguages(true);
+        if(key_exists($this->languageManager->getCurrentLanguage()->getId(), $languages)){
+          $block_didomi = $block_didomi->getTranslation($this->languageManager->getCurrentLanguage()->getId());
+        }
         //on récupère le contenu html
         $body_html_block_didomi = $block_didomi->get('body')->getValue();
+
         if(count($body_html_block_didomi) > 0) {
           $body_html_block_didomi = reset($body_html_block_didomi);
+
           if(isset($body_html_block_didomi['value'])){
             //on crée l'element pour afficher le contenu du block
-            $new_block_element = $html->createDocumentFragment();
-            $new_block_element->appendXML($body_html_block_didomi['value']);
             $div_parent_block = $html->createElement('div');
             $div_parent_block->setAttribute('class', 'divParentBlockMessageCookie');
             $div_parent_block->setAttribute('style', 'display: none;');
-            $div_parent_block->appendChild($new_block_element);
+
+            // la div didomi-message : case noire au fond blanc
+            $div_didomi_message = $html->createElement('div');
+            $div_didomi_message->setAttribute('class', 'didomi-message');
+            //html du block => dans la div didomi-message
+            $new_block_element = $html->createDocumentFragment();
+            $new_block_element->appendXML($body_html_block_didomi['value']);
+            $div_didomi_message->appendChild($new_block_element);
+
+            $a_button = $html->createElement('a');
+            $a_button->setAttribute('class', 'btn btn-default btn_accept_cookie_youtube');
+            $a_button->textContent = t('Accept');
+
+            $div_didomi_message->appendChild($a_button);
+
+            $div_parent_block->appendChild($div_didomi_message);
 
             //on crée une div parent pour y mettre l'iframe + le nouveau block
             $super_div_parent = $html->createElement('div');
