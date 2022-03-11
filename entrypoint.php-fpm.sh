f4d0924ba73df9cdfa89237da3fd2f0a26f3a629
Adrien <adrien.thevenet@otange.com>
2022-03-11 17:10:00 +0100

    RUBYPORTAILOBS-3885 essai entrypoint sans exec

--- entrypoint.php-fpm.sh
+++ entrypoint.php-fpm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 php-fpm --daemonize
-exec yarn --cwd themes/theme_one_i install
-exec yarn --cwd themes/theme_one_i watch
+yarn --cwd themes/theme_one_i install
+yarn --cwd themes/theme_one_i watch
