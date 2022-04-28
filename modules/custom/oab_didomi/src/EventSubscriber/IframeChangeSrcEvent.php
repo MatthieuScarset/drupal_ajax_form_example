9f8ca6c538dabdb03972d6c19359cb834128a262
stephanie <stephanie.jourdan@orange.com>
2022-04-28 10:32:26 +0200

    RUBYPORTAILOBS-3916 > correction module didomi s'il n'y a pas de contenu

--- modules/custom/oab_didomi/src/EventSubscriber/IframeChangeSrcEvent.php
+++ modules/custom/oab_didomi/src/EventSubscriber/IframeChangeSrcEvent.php
@@ -50,68 +50,75 @@ class IframeChangeSrcEvent implements EventSubscriberInterface {
 
   public function onResponse(ResponseEvent $event) {
     $html = new \DOMDocument();
-    @$html->loadHTML($event->getResponse()->getContent());
+    $content = $event->getResponse()?->getContent();
+    if(!empty($content)) {
+      @$html->loadHTML($content);
+      if (!empty($html)) {
+        $xpath = new \DOMXPath($html);
+        /** @var \DOMElement $iframe */
+        foreach ($xpath->query("//iframe[(contains(@src, 'youtube'))]") as $iframe) {
+          //on change les attributs de l'iframe youtube pour passer le src en data-src
+          $iframe->setAttribute('data-src', $iframe->getAttribute('src'));
+          $iframe->removeAttribute('src');
+          $iframe->setAttribute('style', 'display: none;');
 
-    $xpath = new \DOMXPath($html);
-    /** @var \DOMElement $iframe */
-    foreach($xpath->query("//iframe[(contains(@src, 'youtube'))]") as $iframe) {
+          //on load le block didomi contenant le message à afficher et le bouton pour accepter le cookie
+          $block_didomi = $this->entityTypeManager->getStorage('block_content')
+            ->loadByProperties(['uuid' => 'eb454a17-56da-4d21-b3ca-565449d7c452']);
 
-      //on change les attributs de l'iframe youtube pour passer le src en data-src
-      $iframe->setAttribute('data-src', $iframe->getAttribute('src'));
-      $iframe->removeAttribute('src');
-      $iframe->setAttribute('style', 'display: none;');
+          if (count($block_didomi) > 0) {
+            $block_didomi = reset($block_didomi);
+            /** @var BlockContent $block_didomi */
+            $languages = $block_didomi->getTranslationLanguages(TRUE);
+            if (key_exists($this->languageManager->getCurrentLanguage()
+              ->getId(), $languages)) {
+              $block_didomi = $block_didomi->getTranslation($this->languageManager->getCurrentLanguage()
+                ->getId());
+            }
+            //on récupère le contenu html
+            $body_html_block_didomi = $block_didomi->get('body')->getValue();
 
-      //on load le block didomi contenant le message à afficher et le bouton pour accepter le cookie
-      $block_didomi = $this->entityTypeManager->getStorage('block_content')->loadByProperties(['uuid' => 'eb454a17-56da-4d21-b3ca-565449d7c452' ]);
+            if (count($body_html_block_didomi) > 0) {
+              $body_html_block_didomi = reset($body_html_block_didomi);
 
-      if(count($block_didomi) > 0) {
-        $block_didomi = reset($block_didomi);
-        /** @var BlockContent $block_didomi */
-        $languages = $block_didomi->getTranslationLanguages(true);
-        if(key_exists($this->languageManager->getCurrentLanguage()->getId(), $languages)){
-          $block_didomi = $block_didomi->getTranslation($this->languageManager->getCurrentLanguage()->getId());
-        }
-        //on récupère le contenu html
-        $body_html_block_didomi = $block_didomi->get('body')->getValue();
+              if (isset($body_html_block_didomi['value'])) {
+                //on crée l'element pour afficher le contenu du block
+                $div_parent_block = $html->createElement('div');
+                $div_parent_block->setAttribute('class', 'divParentBlockMessageCookie');
+                $div_parent_block->setAttribute('style', 'display: none;');
 
-        if(count($body_html_block_didomi) > 0) {
-          $body_html_block_didomi = reset($body_html_block_didomi);
+                // la div didomi-message : case noire au fond blanc
+                $div_didomi_message = $html->createElement('div');
+                $div_didomi_message->setAttribute('class', 'didomi-message');
+                //html du block => dans la div didomi-message
+                $new_block_element = $html->createDocumentFragment();
+                $new_block_element->appendXML($body_html_block_didomi['value']);
+                $div_didomi_message->appendChild($new_block_element);
 
-          if(isset($body_html_block_didomi['value'])){
-            //on crée l'element pour afficher le contenu du block
-            $div_parent_block = $html->createElement('div');
-            $div_parent_block->setAttribute('class', 'divParentBlockMessageCookie');
-            $div_parent_block->setAttribute('style', 'display: none;');
+                $a_button = $html->createElement('a');
+                $a_button->setAttribute('class', 'btn btn-default btn_accept_cookie_youtube');
+                $a_button->textContent = t('Accept');
 
-            // la div didomi-message : case noire au fond blanc
-            $div_didomi_message = $html->createElement('div');
-            $div_didomi_message->setAttribute('class', 'didomi-message');
-            //html du block => dans la div didomi-message
-            $new_block_element = $html->createDocumentFragment();
-            $new_block_element->appendXML($body_html_block_didomi['value']);
-            $div_didomi_message->appendChild($new_block_element);
+                $div_didomi_message->appendChild($a_button);
 
-            $a_button = $html->createElement('a');
-            $a_button->setAttribute('class', 'btn btn-default btn_accept_cookie_youtube');
-            $a_button->textContent = t('Accept');
+                $div_parent_block->appendChild($div_didomi_message);
 
-            $div_didomi_message->appendChild($a_button);
+                //on crée une div parent pour y mettre l'iframe + le nouveau block
+                $super_div_parent = $html->createElement('div');
+                $super_div_parent->setAttribute('class', 'divParentIframeYoutube');
+                $super_div_parent->setAttribute('is', 'div-iframe-youtube');
+                $parentNode = $iframe->parentNode;
+                $super_div_parent->appendChild($iframe);
+                $super_div_parent->appendChild($div_parent_block);
+                $parentNode->appendChild($super_div_parent);
 
-            $div_parent_block->appendChild($div_didomi_message);
-
-            //on crée une div parent pour y mettre l'iframe + le nouveau block
-            $super_div_parent = $html->createElement('div');
-            $super_div_parent->setAttribute('class', 'divParentIframeYoutube');
-            $super_div_parent->setAttribute('is', 'div-iframe-youtube');
-            $parentNode = $iframe->parentNode;
-            $super_div_parent->appendChild($iframe);
-            $super_div_parent->appendChild($div_parent_block);
-            $parentNode->appendChild($super_div_parent);
-
-            //on save le nouveau html formé
-            $event->getResponse()->setContent($xpath->document->saveHTML());
+                //on save le nouveau html formé
+                $event->getResponse()->setContent($xpath->document->saveHTML());
+              }
+            }
           }
         }
+
       }
     }
   }
