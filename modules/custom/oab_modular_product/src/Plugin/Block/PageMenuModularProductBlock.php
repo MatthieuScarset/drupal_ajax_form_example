c27af29f50c04f1674016edfba36cae8629e71bf
stephanie <stephanie.jourdan@orange.com>
2022-05-03 15:11:56 +0200

    avancement du page menu

--- modules/custom/oab_modular_product/src/Plugin/Block/PageMenuModularProductBlock.php
+++ modules/custom/oab_modular_product/src/Plugin/Block/PageMenuModularProductBlock.php
@@ -11,6 +11,7 @@ use Drupal\media\Entity\Media;
 use Drupal\node\Entity\Node;
 use Drupal\paragraphs\Entity\Paragraph;
 use Drupal\taxonomy\Entity\Term;
+use Symfony\Contracts\Translation\TranslatorTrait;
 
 /**
  *
@@ -22,7 +23,8 @@ use Drupal\taxonomy\Entity\Term;
  *   context_definitions = {
  *     "node" = @ContextDefinition(
  *       "entity:node",
- *       label = @Translation("Current Node")
+ *       label = @Translation("Current Node"),
+ *       required = FALSE,
  *     )
  *   }
  * )
@@ -35,7 +37,6 @@ class PageMenuModularProductBlock extends BlockBase {
    */
   public function access(AccountInterface $account, $return_as_object = FALSE) {
     $node = $this->getContextValue('node');
-
     if ($node instanceof Node && $node->bundle() === "modular_product") {
       return parent::access($account, $return_as_object);
     }
@@ -48,81 +49,60 @@ class PageMenuModularProductBlock extends BlockBase {
   public function build(): array {
     $block = [];
     $node = $this->getContextValue('node');
-    if ($node instanceof Node && $node->bundle() === "modular_product") {
-        if (!empty($node->field_presentation)) {
-        $values_presentation = $node->field_presentation->getValue();
-        foreach ( $values_presentation as $element ) {
-          if (!empty($element['target_id'])) {
-            $paragraphEntity = Paragraph::load($element['target_id']);
-            if(!empty($paragraphEntity)) {
-              if ($paragraphEntity->bundle() == 'module_presentation') {
-                $block['children_items'][] = [
-                  'id' => 'presentation',
-                  'label' => t('Presentation')
-                ];
-              }
-            }
-          }
+
+    // PRESENTATION
+    $paragraphEntity = Paragraph::load($node->field_presentation->target_id ?? 0);
+    if(!empty($paragraphEntity)) {
+      if ($paragraphEntity->bundle() == 'module_presentation') {
+        $block['children_items'][] = [
+          'id' => 'presentation',
+          'label' => $this->t('Presentation'),
+        ];
+      }
+    }
+
+
+    //DETAIL OFFRE/GAMME
+    $paragraphEntity = Paragraph::load($node->field_detail_offre->target_id ?? 0);
+    if(!empty($paragraphEntity)) {
+      if($paragraphEntity->bundle() == 'module_detail_offre') {
+        $block['children_items'][] = [
+          'id' => 'detail-offre',
+          'label' => $this->t('Offer Detail'),
+        ];
+      }
+      if($paragraphEntity->bundle() == 'module_detail_gamme') {
+        $block['children_items'][] = [
+          'id' => 'detail-gamme',
+          'label' => $this->t('Detail of the range'),
+        ];
+      }
+    }
+
+    //MODULES
+    /** @var \Drupal\Core\Field\FieldItemList $module */
+    foreach ( $node->field_modules as $module ) {
+      $paragraphModuleEntity = Paragraph::load($module->target_id ?? 0);
+      if(!empty($paragraphModuleEntity)) {
+        if($paragraphModuleEntity->bundle() == 'module_services') {
+          $block['children_items'][] = [
+            'id' => 'services',
+            'label' => $this->t('Tailor-made services'),
+          ];
+        }
+        if($paragraphModuleEntity->bundle() == 'module_exemples') {
+          $block['children_items'][] = [
+            'id' => 'exemple',
+            'label' => $this->t('Examples'),
+          ];
+        }
+        if($paragraphModuleEntity->bundle() == 'module_testimonial') {
+          $block['children_items'][] = [
+            'id' => 'temoignages',
+            'label' => $this->t('Testimonials'),
+          ];
         }
       }
-
-      if ($node->hasField('field_detail_offre') && !empty($node->field_detail_offre)) {
-        $values_detail_offre = $node->field_detail_offre->getValue();
-        foreach ( $values_detail_offre as $element ) {
-          if(!empty($element['target_id'])) {
-            $paragraphEntity = Paragraph::load($element['target_id']);
-            if(!empty($paragraphEntity)) {
-              if($paragraphEntity->bundle() == 'module_detail_offre') {
-                $block['children_items'][] = [
-                  'id' => 'detail-offre',
-                  'label' => t('Offer Detail')
-                ];
-              }
-              if($paragraphEntity->bundle() == 'module_detail_gamme') {
-                $block['children_items'][] = [
-                  'id' => 'detail-gamme',
-                  'label' => t('Detail of the range')
-                ];
-              }
-            }
-          }
-        }
-      }
-      if (!empty($node->field_modules)) {
-        $values_modules = $node->field_modules->getValue();
-        foreach ( $values_modules as $module ) {
-          if(!empty($module['target_id'])) {
-            $paragraphModuleEntity = Paragraph::load($module['target_id']);
-            if(!empty($paragraphModuleEntity)) {
-              if($paragraphModuleEntity->bundle() == 'module_services') {
-                $block['children_items'][] = [
-                  'id' => 'services',
-                  'label' => t('Tailor-made services')
-                ];
-              }
-              if($paragraphModuleEntity->bundle() == 'module_exemples') {
-                $block['children_items'][] = [
-                  'id' => 'exemple',
-                  'label' => t('Example of realization')
-                ];
-              }
-              if($paragraphModuleEntity->bundle() == 'module_testimonial') {
-                $block['children_items'][] = [
-                  'id' => 'temoignages',
-                  'label' => t('Testimonials')
-                ];
-              }
-            }
-          }
-        }
-      }
-
-      //parser les modules du field_modules
-      // vérifier type du paragraph
-      // pour ajouter lien si Services exclusifs / exemples / témoignages
-
-      //regarder où on prend les libellés
-
     }
     return $block;
   }
