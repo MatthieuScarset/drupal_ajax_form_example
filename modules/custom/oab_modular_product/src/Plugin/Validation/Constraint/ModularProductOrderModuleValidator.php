22055ec7f7505e0fe4e7a075196137e0c1b4205e
mourchid <mourchid.salifou@orange.com>
2022-05-10 11:56:32 +0200

    RUBYPORTAILOBS-3900 Export conf suppression du field illustration du module exemple, modification du template et js pour la boucle infini

--- modules/custom/oab_modular_product/src/Plugin/Validation/Constraint/ModularProductOrderModuleValidator.php
+++ modules/custom/oab_modular_product/src/Plugin/Validation/Constraint/ModularProductOrderModuleValidator.php
@@ -16,6 +16,7 @@ class ModularProductOrderModuleValidator extends ConstraintValidator {
    */
   public function validate($items, Constraint $constraint) {
 
+//liste de modules optionnels de la page produit dans le bon ordre
     $default_order = [
       "module_services",
       "module_steps",
@@ -26,36 +27,42 @@ class ModularProductOrderModuleValidator extends ConstraintValidator {
       "module_testimonial"
     ];
 
+//Liste des modules supplémentaires optionnels dont on ne tiendra pas compte
     $not_in_order = [
       "module_3_4_colonnes",
       "module_text_video_image",
       "paragraph_wysiwyg",
     ];
 
-    $paragraph_order = [];
 
+//On récupère les modules ajoutés en BO en excluant les modules supplémentaires
+    $paragraph_order = [];
     foreach ($items as $item) {
       if (isset($item->target_id)) {
         $paragraph = Paragraph::load($item->target_id ?? 0);
-        if ($paragraph) {
-          if (!in_array($paragraph->bundle(), $not_in_order)) {
-            $paragraph_order[] = $paragraph->bundle();
-          }
+        if ($paragraph && !in_array($paragraph->bundle(), $not_in_order)) {
+          $paragraph_order[] = $paragraph->bundle();
         }
       }
     }
 
+//On boucle sur nos tableaux pour les comparés
     $i_current = 0;
-
-    while (count($default_order) && count($paragraph_order) && isset($paragraph_order[$i_current])) {
+    while (count($default_order) && count($paragraph_order)
+            && isset($paragraph_order[$i_current])) {
+//On dépile le tableau de départ
       $current_order = array_shift($default_order);
+
+//On passe à un autre élément que s'il y a égalité entre les premiers éléments
+  //en incrémentant notre compteur
       if ($current_order === $paragraph_order[$i_current]) {
         $i_current++;
       }
     }
-    $ordered = ($i_current === count($paragraph_order));
 
-    if (!$ordered) {
+//Nos deux tableaux sont égaux que s'il y a égalité entre le compteur et le
+  //nombre d'éléments dans le tableau récupérer en BO
+    if ($i_current !== count($paragraph_order)) {
       $this->context->addViolation($constraint->badOrder);
     }
   }
