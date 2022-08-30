cf2d371294d20e910a58aea21d94471b0fe8ab64
mourchid <mourchid.salifou@orange.com>
2022-08-30 13:51:31 +0200

    RUBYPORTAILOBS-4010 Ajout scss dans le thème backoffice claro

---
+++ themes/theme_obs_backoffice_claro/webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  mode: 'production',
+  stats: 'normal',
+  entry: {
+    "css/style": './scss/style.scss',
+    "css/components": './scss/components.scss',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'assets/'),
+    publicPath: "assets/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              url: false,
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  ["autoprefixer"]
+                ]
+              }
+            }
+          },
+          {loader: 'sass-loader'},
+          {loader: 'webpack-import-glob-loader'}
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({test: /\.js(\?.*)?$/i})],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ]
+};
