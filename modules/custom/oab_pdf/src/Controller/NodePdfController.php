57a630dc831c7c63e69a8d43376c868a76a04f62
Tristan leuillier <tristan.leuillier@orange.com>
2023-02-09 08:55:01 +0100

    RUBYPORTAILOBS-4208 > Edit suite MR | Remise des templates dans les bons dossiers | Ajout d'une validation des droits pour l'accès aux PDF | Ajout de la gestion d'un cache pour éviter de rebuild les PDF | Création d'exceptions et messages d'erreurs | Ajout d'une factory + mise en service de Snappy

--- modules/custom/oab_pdf/src/Controller/NodePdfController.php
+++ modules/custom/oab_pdf/src/Controller/NodePdfController.php
@@ -2,25 +2,26 @@
 
 namespace Drupal\oab_pdf\Controller;
 
+use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
-use Drupal\Core\Render\RendererInterface;
 use Drupal\node\NodeInterface;
-use Drupal\oab_pdf\Services\OabPdfGeneratorService;
+use Drupal\oab_pdf\Exception\SnappyException;
+use Drupal\oab_pdf\PdfGenerator\NodePdfGenerator;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class NodePdfController extends ControllerBase implements ContainerInjectionInterface {
 
-  public function __construct(protected OabPdfGeneratorService $oab_pdf_generator_service,
-                              protected RendererInterface $renderer) {
+  public function __construct(private NodePdfGenerator $nodePdfGenerator,
+                              private CacheBackendInterface $cacheBackend) {
   }
 
   public static function create(ContainerInterface $container) {
     return new self(
-      $container->get('oab_pdf.pdf_generator'),
-      $container->get('renderer')
+      $container->get('oab_pdf.node_pdf_generator'),
+      $container->get('oab_pdf.pdf_cache')
     );
   }
 
@@ -28,16 +29,32 @@ class NodePdfController extends ControllerBase implements ContainerInjectionInte
    * @throws \Exception
    */
   public function getNodePdf(NodeInterface $node): Response {
-    if ($node->bundle() !== "mss_article") {
-      // Si ce n'est pas un article MSS => 404 Not Found
+
+    if ($node->bundle() !== "mss_article" || $node->field_typology?->value !== "article") {
+      // Temporaire -- Si ce n'est pas un article MSS => 404 Not Found
       throw new NotFoundHttpException($this->t("Page not found"));
     }
-    $build = $this->entityTypeManager()->getViewBuilder($node->getEntityTypeId())->view($node, 'pdf');
-    $markup = $this->renderer->renderRoot($build);
-    $content_response = $this->oab_pdf_generator_service->getOutput($markup, $node->label());
-   // dd($markup);
-    $response = new Response($content_response);
-    $response->headers->set('Content-Type', 'application/pdf');
-    return $response;
+
+    $cid = "node_pdf_{$node->id()}";
+    try {
+      if (($cached_data = $this->cacheBackend->get($cid)) !== false) {
+        $content = $cached_data->data;
+      } else {
+        $content = $this->nodePdfGenerator->generatePdfContent($node);
+        $this->cacheBackend->set($cid, $content, $node->getCacheMaxAge(), $node->getCacheTags());
+      }
+      return new Response(
+        $content,
+        headers: ['Content-type' => 'application/pdf']
+      );
+    } catch (SnappyException $e) {
+      $this->getLogger('oab_pdf')->alert($e->getMessage());
+    }
+
+    // Si on est là, c'est qu'on n'a pas retourné le PDF plus haut. Retour en 400
+    return new Response(
+      $this->t("Something went wrong while generating PDF for @node.", ['@node' => $node->label()]),
+      400
+    );
   }
 }
