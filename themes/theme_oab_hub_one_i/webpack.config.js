643347301b270167c37753673a0de009cfcbb4a8
mourchid <mourchid.salifou@orange.com>
2023-03-06 16:09:37 +0100

    RUBYPORTAILOBS-3878 Merge du ticket pdf pour avoir le template du boutton pour télécharger le pdf dans le template du thème oab-hub-one-i et modif du page title

--- themes/theme_oab_hub_one_i/webpack.config.js
+++ themes/theme_oab_hub_one_i/webpack.config.js
@@ -1,17 +1,63 @@
-const path = require('path')
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const webpackConfig = require('../theme_one_i/webpack.common');
-
-webpackConfig.mode = 'production';
-
-webpackConfig.entry = {
-  "css/style": './assets/scss/style.scss',
-};
-
-webpackConfig.output = {
-  filename: '[name].js',
+module.exports = {
+  mode: 'production',
+  stats: 'normal',
+  entry: {
+    "css/style": './assets/scss/style.scss',
+  },
+  output: {
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist/'),
     publicPath: "/themes/theme_oab_hub_one_i/dist/"
-}
-
-module.exports = webpackConfig;
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  ["autoprefixer"]
+                ]
+              }
+            }
+          },
+          'resolve-url-loader',
+          {loader: 'sass-loader', options: { sourceMap: true}},
+          'webpack-import-glob-loader'
+        ],
+      },
+      {
+        test: /\.(woff2?|ttf|otf|eot|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: 'fonts/[name].[ext]'
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ],
+  externals: {
+    jquery: 'jQuery',
+  },
+  watchOptions: {
+    poll: true,
+    ignored: /node_modules/
+  }
+};
