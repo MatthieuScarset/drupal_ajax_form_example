b74db7c97f38ef06fb151095630c3761965645bc
Adrien <adrien.thevenet@otange.com>
2023-04-12 17:09:03 +0200

    RUBYPORTAILOBS-4243 ajout dans la conf de ckeditor de la possibilité de mettre des div, p, i et span dans des <a>

--- modules/custom/oab_ckeditor/js/custom_config.js
+++ modules/custom/oab_ckeditor/js/custom_config.js
@@ -14,4 +14,11 @@ CKEDITOR.editorConfig = function(config) {
 
     CKEDITOR.config.embed_provider = '//iframe.ly/api/oembed?url={url}&callback={callback}&api_key=999d16e5ad074182bbe882';
     /*CKEDITOR.config.removePlugins = 'drupallink';*/
+
+    CKEDITOR.dtd['a'] = {
+      div: 1,
+      p: 1,
+      i: 1,
+      span: 1
+    };
 };
