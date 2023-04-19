f4dc1a48991a4271f45822d4c592a50be799cbdb
Adrien <adrien.thevenet@otange.com>
2023-04-19 16:40:21 +0200

    RUBYPORTAILOBS-4358 retour MR, creation breadcrumb builder node simple, factorisation disable wysiwyg

--- modules/custom/oab_frontoffice/src/Breadcrumb/OabFrontofficBreadcrumbBuilder.php
+++ modules/custom/oab_frontoffice/src/Breadcrumb/OabFrontofficBreadcrumbBuilder.php
@@ -178,18 +178,13 @@ class OabFrontofficBreadcrumbBuilder implements BreadcrumbBuilderInterface {
                         }
                     }
                 }
-            } elseif (isset($node)
-              && method_exists(get_class($node), 'getType')
-              && in_array($node->getType(), ['cloud_service_provider', 'corporate'])) {
-
-              $breadcrumb->addLink(Link::fromTextAndUrl(t('Home'), $this->getHubFrontUrl()));
-
+            }
+            elseif (isset($node)
+              && method_exists(get_class($parameters['node']), 'getType')
+              && $parameters['node']->getType() == 'cloud_service_provider') {
               //ajout du fil d'ariane pour les CSP de BVPN
-              if ($node->getType() === 'cloud_service_provider') {
-                $breadcrumb->addLink(Link::createFromRoute(t('BVPN Galerie - List of Cloud Services'), 'view.bvpn_gallery.csp_list_page'));
-              } else { //ajout du fil d'ariane pour les pages Corporates
-                $breadcrumb->addLink(Link::createFromRoute($node->getTitle(), '<none>'));
-              }
+              $breadcrumb->addLink(Link::fromTextAndUrl(t('Home'), $this->getHubFrontUrl()));
+              $breadcrumb->addLink(Link::createFromRoute(t('BVPN Galerie - List of Cloud Services'), 'view.bvpn_gallery.csp_list_page'));
             }
         }
 
