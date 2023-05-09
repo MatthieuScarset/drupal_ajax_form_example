099cee3614efa00f9e7f532583257d7fb5da65aa
Adrien <adrien.thevenet@otange.com>
2023-05-09 14:32:08 +0200

    RUBYPORTAILOBS-4389 retour MR simplification gestion thème ob1 CT

--- modules/custom/oab_frontoffice/modules/oab_ob1_adapter/src/Theme/Ob1ContentThemeNegotiator.php
+++ modules/custom/oab_frontoffice/modules/oab_ob1_adapter/src/Theme/Ob1ContentThemeNegotiator.php
@@ -4,9 +4,7 @@ namespace Drupal\oab_ob1_adapter\Theme;
 
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Theme\ThemeNegotiatorInterface;
-use Drupal\node\Entity\NodeType;
 use Drupal\node\NodeInterface;
-use Drupal\node\NodeTypeInterface;
 
 
 class Ob1ContentThemeNegotiator extends AbstractOb1ThemeNegotiator implements ThemeNegotiatorInterface {
@@ -17,29 +15,9 @@ class Ob1ContentThemeNegotiator extends AbstractOb1ThemeNegotiator implements Th
     $current_node = $route_match->getParameter('node');
 
     if ($current_node) {
-      /** @var NodeTypeInterface $node_type */
-      $node_type = NodeType::load($current_node->bundle());
-      $ob1 = $node_type->getThirdPartySetting('oab_modular_product', 'ob1_theme');
-
-      if ($ob1 === 1) {
-        return true;
-      }
+      return $current_node->type->entity->getThirdPartySetting('oab_modular_product', 'ob1_theme');
     }
 
-//    // Toutes les routes ou le thème doit s'appliquer
-//    // ie. le front, mais aussi les pages de prévi et revisions
-//    $routes_to_apply = [
-//      'entity.node.latest_version',
-//      'entity.node.canonical',
-//      'entity.node.revision'
-//    ];
-//
-//    if (isset($current_node) && in_array($route_match->getRouteName(), $routes_to_apply)) {
-//        return
-//          (isset($current_node->field_use_theme_ob1) && $current_node->field_use_theme_ob1->value == 1)
-//          || $this->ob1AdapterService->hasContent($current_node->bundle());
-//    }
-//
     return false;
   }
 }
