ba35bd03d1a44341aaea80bbf5e8f3e6af241ae9
Hela Dhifli <hela.dhifli@orange.com>
2023-06-14 17:25:57 +0200

    RUBYPORTAILOBS-4411: correction d'anomalie pour création et modification module détail de la gamme

--- modules/custom/oab_modular_product/src/Plugin/Validation/Constraint/ModularProductDetailOffreCheckCTAValidator.php
+++ modules/custom/oab_modular_product/src/Plugin/Validation/Constraint/ModularProductDetailOffreCheckCTAValidator.php
@@ -20,28 +20,31 @@ class ModularProductDetailOffreCheckCTAValidator extends ConstraintValidator {
       if (isset($item->entity)) {
         /** @var \Drupal\paragraphs\Entity\Paragraph $entity */
         $entity = $item->entity;
-        $value_cta_detail_offre_global = $item->entity->get('field_call_to_action_buton')->getValue();
-        if (!empty($value_cta_detail_offre_global[0]["title"])) {
-          //LE CTA globale est rempli au niveau du module détail Ofrre => on doit vérifier les enfants
-          
-          if (!empty($entity->field_items)) {
-            $bool_error = FALSE;
-            $i = 0;
-            //tant qu'il y a des enfants et qu'il n'y a pas d'erreur
-            foreach ($entity->field_items as $field_items) {
-              if (!$bool_error) {
-                $offre_item =  $field_items->subform;
+        if($item->entity->hasField('field_call_to_action_buton')) {
+          $value_cta_detail_offre_global = $item->entity->get('field_call_to_action_buton')->getValue();
+          if (!empty($value_cta_detail_offre_global[0]["title"])) {
+            //LE CTA globale est rempli au niveau du module détail Ofrre => on doit vérifier les enfants
 
-                if ($offre_item['field_cta'] !== NULL && count($offre_item['field_cta']) > 1) {
-                  // il y a un CTA sur l'item => on renvoie une erreur
-                  $bool_error = TRUE;
-                  $this->context->addViolation($constraint->error);
+            if (!empty($entity->field_items)) {
+              $bool_error = FALSE;
+              $i = 0;
+              //tant qu'il y a des enfants et qu'il n'y a pas d'erreur
+              foreach ($entity->field_items as $field_items) {
+                if (!$bool_error) {
+                  $offre_item =  $field_items->subform;
+
+                  if ($offre_item['field_cta'] !== NULL && count($offre_item['field_cta']) > 1) {
+                    // il y a un CTA sur l'item => on renvoie une erreur
+                    $bool_error = TRUE;
+                    $this->context->addViolation($constraint->error);
+                  }
+                  $i++;
                 }
-                $i++;
               }
             }
           }
         }
+
       }
     }
   }
