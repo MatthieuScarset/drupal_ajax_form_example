53bb5da43fc5002f750fb60071f53b898067dcd8
Hela Dhifli <hela.dhifli@orange.com>
2023-06-16 16:25:16 +0200

    RUBYPOTTAILOBS-4411: refacto code pour checkCTA

--- modules/custom/oab_modular_product/src/Plugin/Validation/Constraint/ModularProductDetailOffreCheckCTAValidator.php
+++ modules/custom/oab_modular_product/src/Plugin/Validation/Constraint/ModularProductDetailOffreCheckCTAValidator.php
@@ -20,17 +20,18 @@ class ModularProductDetailOffreCheckCTAValidator extends ConstraintValidator {
       if (isset($item->entity)) {
         /** @var \Drupal\paragraphs\Entity\Paragraph $entity */
         $entity = $item->entity;
-        if (!empty($entity->field_call_to_action_buton->title)) {
+        if (!empty($entity->field_call_to_action_buton->title) && !empty($entity->field_items)) {
           //LE CTA globale est rempli au niveau du module détail Ofrre => on doit vérifier les enfants
-          if (!empty($entity->field_items)) {
-            //tant qu'il y a des enfants et qu'il n'y a pas d'erreur
-            foreach ($entity->field_items as $field_items) {
-                $offre_item =  $field_items->subform;
-                if ($offre_item['field_cta'] !== NULL && count($offre_item['field_cta']) > 1) {
-                  // il y a un CTA sur l'item => on renvoie une erreur
-                  $this->context->addViolation($constraint->error);
-                }
-            }
+          //tant qu'il y a des enfants et qu'il n'y a pas d'erreur
+          foreach ($entity->field_items as $field_item) {
+              $offre_item =  $field_item->subform;
+              //field_cta n'est jamais vide et contient par défaut (sans ajouter un CTA) une valeur d'ou le count > 1
+              if (isset($offre_item['field_cta']) && count($offre_item['field_cta']) > 1) {
+                // il y a un CTA sur l'item => on renvoie une erreur
+                $this->context->addViolation($constraint->error);
+              } elseif ($field_item->entity->field_cta->count() > 1) {
+                $this->context->addViolation($constraint->error);
+              }
           }
         }
       }
