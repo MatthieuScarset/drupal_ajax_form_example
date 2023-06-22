54c25a5fab3ac2af6559f6fc0d1a49edab7e6654
Adrien <adrien.thevenet@otange.com>
2023-06-22 14:57:36 +0200

    RUBYPORTAILOBS-4261 finalisation css + js storytelling

--- themes/theme_one_i/assets/js/modular_content/module-storytelling.js
+++ themes/theme_one_i/assets/js/modular_content/module-storytelling.js
@@ -3,53 +3,25 @@ import ModuleExempleSlider from "./module-exemple-slider";
 class ModuleStorytelling {
 
   constructor(elem) {
-    this.$root = elem;
-    this.$storyPanel = elem.querySelector('.right-panel');
-    this.$storyParts = this.$root.querySelectorAll('.storypart');
-    this.firstStoryPart = this.$root.querySelector('#story1');
+    this.$storyParts = elem.querySelectorAll('.storypart');
 
-    this.firstStoryPart.classList.add('active');
-    this.firstStoryPart.querySelector('.item-icon').classList.toggle('d-mb-block');
-    this.firstStoryPart.querySelector('.item-icon').classList.toggle('d-none');
+    this.$observer =  new IntersectionObserver(function(entries) {
+      entries.forEach((entry) => {
+        if(entry.isIntersecting) {
+          elem.querySelectorAll('.items-list-item').forEach((storyList) => {
+            if (storyList.dataset.storyTitleId !== entry.target.dataset.storyId) {
+              storyList.classList.remove('active');
+              storyList.querySelector('.item-icon').classList.add('d-none');
+            } else {
+              storyList.classList.add('active');
+              storyList.querySelector('.item-icon').classList.remove('d-none');
+            }
+          });
+        }
+      });
+    }, { threshold: [0.70] });
 
-    this.$root.querySelector('.right-panel').addEventListener('scroll', () => {
-      this._changeActiveIcon();
-    });
-  }
-
-  _isActive() {
-    let indexElemVisible = 1;
-
-    this.$storyParts.forEach((storyPart, index) => {
-      const rect = storyPart.getBoundingClientRect();
-      const topPosition = this.$storyPanel.getBoundingClientRect().top;
-
-      let elemStyle =  window.getComputedStyle(storyPart);
-      let elemPaddingBottom = parseInt(elemStyle.paddingBottom)
-
-      //verify if the element is visible
-      if ((rect.bottom - elemPaddingBottom) > topPosition && (rect.top - elemPaddingBottom)  <= topPosition) {
-        indexElemVisible = index+1;
-      }
-    });
-
-    return indexElemVisible;
-  }
-
-  _changeActiveIcon() {
-    let storyActive = this.$root.querySelector('#story' + this._isActive());
-
-    this.$root.querySelectorAll('.items-list-item').forEach((storyList) => {
-      if (storyList !== storyActive.id) {
-        storyList.classList.remove('active');
-        storyList.querySelector('.item-icon').classList.remove('d-mb-block');
-        storyList.querySelector('.item-icon').classList.add('d-none');
-      }
-    });
-
-    storyActive.classList.add('active');
-    storyActive.querySelector('.item-icon').classList.toggle('d-mb-block');
-    storyActive.querySelector('.item-icon').classList.toggle('d-none');
+    this.$storyParts.forEach((el) => this.$observer.observe(el));
   }
 }
 
