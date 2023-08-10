27bdd0a3f225396aa9321f42d86618267fa0ef7c
Hela Dhifli <hela.dhifli@orange.com>
2023-08-10 16:18:51 +0200

    RUBYPORTAILOBS-4421 : Fix alignement premiers key points en mobile

--- themes/theme_one_i/assets/js/modular_content/custom-alignment.js
+++ themes/theme_one_i/assets/js/modular_content/custom-alignment.js
@@ -2,23 +2,31 @@ class CustomAlignment {
   constructor(root) {
     this.$root = root;
     this.$moduleItems = Array.from(this.$root.querySelectorAll('.horizontal-align-item'));
+    this.$slideItemsInMobile = this.$root.querySelector('.slide-mobile');
 
-    $(window).resize(()=> {
+    $(window).resize(() => {
       this._setHeighToAlign();
     });
 
     this._setHeighToAlign();
   }
-
   _setHeighToAlign() {
     let heightToAlign = 0;
-    if (window.matchMedia("(min-width: 736px)").matches) {
+
+    if ((this.$slideItemsInMobile || window.matchMedia("(min-width: 736px)").matches)) {
+
       this.$moduleItems.forEach((item) => {
-        heightToAlign = Math.max(heightToAlign, item.querySelector('.horizontal-align-item-field').offsetHeight);
+        if(item.querySelector('.horizontal-align-item-field') !== null) {
+          heightToAlign = Math.max(heightToAlign, item.querySelector('.horizontal-align-item-field').offsetHeight);
+        }
       });
+
       this.$moduleItems.forEach((item) => {
-        item.querySelector('.horizontal-align-item-field').style.minHeight = `${heightToAlign}px`;
+        if(item.querySelector('.horizontal-align-item-field') !== null) {
+          item.querySelector('.horizontal-align-item-field').style.minHeight = `${heightToAlign}px`;
+        }
       });
+
     }
   }
 }
