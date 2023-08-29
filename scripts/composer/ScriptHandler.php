bc9b22705939008f34c793dc2396d36607bc60f7
Matthieu <m@matthieuscarset.com>
2023-08-29 14:16:59 +0200

    RUBYPORTAILOBS-4458 > Fix args in script handler

--- scripts/composer/ScriptHandler.php
+++ scripts/composer/ScriptHandler.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * @file
- * Contains \DrupalProject\composer\ScriptHandler.
- */
-
 namespace DrupalProject\composer;
 
 use Composer\Script\Event;
@@ -12,13 +7,16 @@ use DrupalFinder\DrupalFinder;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Process\Process;
 
+/**
+ * Handles our custom composer scripts.
+ */
 class ScriptHandler {
 
   /**
    * Helper method to run any executable available in vendor/bin.
-   * 
-   * Process output is logged under log/{$exec}.log
-   * 
+   *
+   * Process output is logged under log/{$exec}.log.
+   *
    * @param \Composer\Script\Event $event
    *   The event.
    * @param string $exec
@@ -38,7 +36,7 @@ class ScriptHandler {
 
     // Execute the process with all arguments.
     $args = $event->getArguments();
-    $process_args = [$binPath] + $args;
+    $process_args = array_merge([$binPath], $args);
     $process = new Process($process_args);
     $process->run();
 
@@ -53,7 +51,7 @@ class ScriptHandler {
 
   /**
    * Handler our custom composer script to run PHPCBF.
-   * 
+   *
    * @param \Composer\Script\Event $event
    *   The event.
    */
@@ -63,11 +61,12 @@ class ScriptHandler {
 
   /**
    * Handler our custom composer script to run PHPCS.
-   * 
+   *
    * @param \Composer\Script\Event $event
    *   The event.
    */
   public static function codeCheck(Event $event) {
     self::runExec($event, 'phpcs');
   }
+
 }
